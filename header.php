88ded9ce48fcb911317e4dcbc8320c42b863a04e
Myzwer <josh@onefortyfivedesign.com>
2023-06-27 16:10:06 -0400

    :art: Made a handful of visual changes to the navbar, swapped logo, changed some colors and size, etc.

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
         <div class="brand">
             <a href="/frontpage">
                 <!--Company Logo / Branding. Template supports both Text and Logo-->
-                <img src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png"
+                <img src="<?php the_field('circle_outline_logo', 'options'); ?>"
                      alt="Company Logo">
             </a>
         </div>
