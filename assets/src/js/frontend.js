4168857c9873831811c0da138a2ffcb8a5199091
Myzwer <josh@onefortyfivedesign.com>
2023-06-29 16:34:41 -0400

    :art: Managed hover states for top bar, as well as active status for the main navbar.

--- assets/src/js/frontend.js
+++ assets/src/js/frontend.js
@@ -1,25 +1,40 @@
 import $ from 'jquery';
 import "../sass/frontend.scss";
 
-  $(function () {
-    // DOM ready
-    // If a link has a dropdown, add sub menu toggle.
-    $("nav ul li a:not(:only-child)").click(function (e) {
-      $(this).siblings(".nav-dropdown").toggle();
-      // Close one dropdown when selecting another
-      $(".nav-dropdown").not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    // Clicking away from dropdown will remove the dropdown class
-    $("html").click(function () {
-      $(".nav-dropdown").hide();
-    });
-    // Toggle open and close nav styles on click
-    $("#nav-toggle").click(function () {
-      $("nav ul").slideToggle();
-    });
-    // Hamburger to X toggle
-    $("#nav-toggle").on("click", function () {
-      this.classList.toggle("active");
-    });
-  }); // end DOM ready
+$(function () {
+  // DOM ready
+
+  // If a link has a dropdown, add sub menu toggle.
+  $("nav ul li a:not(:only-child)").click(function (e) {
+
+    // Remove "active-dropdown" class from other anchor elements
+    $("nav ul li a").not(this).removeClass("active-dropdown");
+
+    // Toggle the "active-dropdown" class on the clicked anchor element
+    $(this).toggleClass("active-dropdown");
+
+    // Toggle the visibility of the sibling dropdown
+    $(this).siblings(".nav-dropdown").toggle();
+
+    // Close one dropdown when selecting another
+    $(".nav-dropdown").not($(this).siblings()).hide();
+
+    e.stopPropagation();
+  });
+
+  // Clicking away from dropdown will remove the dropdown class and active-dropdown class
+  $("html").click(function () {
+    $(".nav-dropdown").hide();
+    $("nav ul li a:not(:only-child)").removeClass("active-dropdown");
+  });
+
+  // Toggle open and close nav styles on click
+  $("#nav-toggle").click(function () {
+    $("nav ul").slideToggle();
+  });
+
+  // Hamburger to X toggle
+  $("#nav-toggle").on("click", function () {
+    this.classList.toggle("active");
+  });
+});
