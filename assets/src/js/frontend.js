e697f196a278ebe1dff21297e5dbeccd0c683207
Myzwer <josh@onefortyfivedesign.com>
2023-06-29 23:45:48 -0400

    :rocket: Changed the functionality of the navbar so that toggleSlide also works for dropdowns, but only on mobile.

--- assets/src/js/frontend.js
+++ assets/src/js/frontend.js
@@ -14,7 +14,16 @@ $(function () {
     $(this).toggleClass("active-dropdown");
 
     // Toggle the visibility of the sibling dropdown
-    $(this).siblings(".nav-dropdown").toggle();
+    const mediaQuery = window.matchMedia("(max-width: 64em)");
+
+    // Check the screen size using the media query
+    if (mediaQuery.matches) {
+      // Mobile: Use slideToggle()
+      $(this).siblings(".nav-dropdown").slideToggle();
+    } else {
+      // Desktop: Use toggle()
+      $(this).siblings(".nav-dropdown").toggle();
+    }
 
     // Close one dropdown when selecting another
     $(".nav-dropdown").not($(this).siblings()).hide();
