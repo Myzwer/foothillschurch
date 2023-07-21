50e5a97301d7be37d7fe9fc4f6805672bf8ecd67
Myzwer <josh@onefortyfivedesign.com>
2023-07-21 01:21:46 -0400

    :rocket: Still sketching out the homepage. Needs testing but it seems to work on desktop on mobile.

--- frontpage.php
+++ frontpage.php
@@ -29,14 +29,15 @@ get_header(); ?>
     </div>
 
     <div class="bg-white-gradient pb-10">
-        <div class="m-4 md:m-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
-            <div class="grid grid-cols-12 gap-4">
+        <div class="lg:max-w-5xl lg:text-center lg:mx-auto p-5 pt-10">
+            <div class="grid grid-cols-12 gap-4 md:gap-10">
                 <div class="col-span-12 md:col-span-6">
                     <img class="rounded-xl shadow-xl" src="<?php the_field("thumbnail"); ?>" alt="">
                 </div>
 
-                <div class="col-span-12 md:col-span-6">
-                    <div class="text-center md:text-left mb-1">
+                <div class="col-span-12 md:col-span-6 relative">
+                    <div class="content-middle-medium">
+                    <div class="text-left mb-1">
                         <h4 class = "uppercase font-semibold">Latest Sermon</h4>
                         <h2 class=" text-xl md:text-3xl lb-2 font-bold capitalize">Get To The Secret
                             Place</h2>
@@ -50,6 +51,38 @@ get_header(); ?>
                         </button>
                     </div>
                 </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-blue-gradient pb-10">
+        <div class=" lg:max-w-5xl lg:text-center lg:mx-auto p-5 pt-10">
+            <div class="grid grid-cols-12 gap-4 md:gap-10">
+
+                <div class="col-span-12 md:col-span-6 md:order-2">
+                    <img src="<?php the_field("resource_pdf"); ?>" alt="">
+                </div>
+
+
+                <div class="col-span-12 md:col-span-6 md:order-1 relative">
+                    <div class="content-middle-medium">
+                        <div class="text-left mb-1">
+                            <h2 class=" text-xl md:text-3xl lb-2 font-bold capitalize">Get a free resource on us!</h2>
+                            <p class = "pb-10 md:pb-3">Learn how to manage your money better with this handy PDF based on Dave Ramsay’s Financial Peace University!</p>
+                            <?php if (have_posts()) : while (have_posts()) : the_post();
+                                the_content();
+                            endwhile;
+                            else: ?>
+                                <p>Sorry, no posts matched your criteria.</p>
+                            <?php endif; ?>
+                            <p class = "opacity-60 text-xs">This site is protected by reCAPTCHA and the Google
+                                <a class = "underline" href="https://policies.google.com/privacy">Privacy Policy</a> and
+                                <a class = "underline" href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
