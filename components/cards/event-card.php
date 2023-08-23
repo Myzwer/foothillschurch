36986c58f4be25f9f361e708933e4064bc461b3a
Myzwer <josh@onefortyfivedesign.com>
2023-08-23 16:32:03 -0400

    :art: Made more cosmetic changes to the cards, cleaned up the events page a little.

--- components/cards/event-card.php
+++ components/cards/event-card.php
@@ -1,8 +1,33 @@
-<div class="col-span-12 md:col-span-6 lg:col-span-4 mx-5 mb-8 bg-gray-light shadow-lg rounded-lg">
+<div class="bg-white col-span-12 md:col-span-6 mx-5 mb-8 bg-gray-light shadow-xl rounded-xl">
     <img class="rounded-t-lg" src="<?php the_field('branding', get_the_ID()); ?>"
-         alt="Sermon Thumbnail">
+         alt="Event Brand">
     <div class="p-5">
-        <h3 class="text-xl md:text-2xl font-bold"><?php the_field('event_name', get_the_ID()); ?></h3>
-        <h3 class="text-xl md:text-2xl font-bold"><?php the_field('event_date', get_the_ID()); ?></h3>
+        <h2 class="text-xl md:text-2xl font-bold capitalize"><?php the_field('event_name', get_the_ID()); ?></h2>
+        <h3 class="text-lg"><?php the_field('event_date', get_the_ID()); ?></h3>
+        <h3 class="text-lg pb-3"><?php the_field('event_start_time', get_the_ID()); ?></h3>
+        <p class="pb-3">
+            <!-- Display the "details" section, but limit the text length-->
+            <?php $trim_length = 140;  //desired length of text to display
+            $custom_field = 'main_details';
+            $value = get_post_meta($post->ID, $custom_field, true);
+            if ($value) {
+                if (strlen($value) > $trim_length)
+                    $value = rtrim(substr($value, 0, $trim_length)) . '...';
+                echo $value;
+            } ?>
+        </p>
+    </div>
+
+    <div class="grid grid-cols-12 rounded-b-xl">
+        <div class="col-span-6 text-center bg-saltydog rounded-bl-xl">
+            <a class="block text-lg uppercase py-3 text-white font-bold" target="_blank"
+               href="<?php the_field('registration_link', get_the_ID()); ?>">Register</a>
+        </div>
+
+        <div class="col-span-6 text-center bg-lightblue rounded-br-xl">
+            <a class="block text-lg uppercase py-3 text-black font-bold"
+               href="<?php the_permalink(); ?>">Learn More</a>
+        </div>
     </div>
 </div>
+
--- events.php
+++ events.php
@@ -13,64 +13,54 @@
 
 get_header(); ?>
 
+    <!-- Simple Header-->
+<?php get_template_part('components/headers/simple-header'); ?>
+    <!-- End Header -->
 
-    <div class="bg-white pb-5">
-        <div class="md:w-9/12 mx-auto grid grid-cols-12 p-5">
-
-            <?php
-            // WP_Query arguments
-            $args = array(
-                'post_type' => array('event'),
-                'post_status' => array('publish'),
-                'nopaging' => false,
-                'order' => 'DESC',
-                'orderby' => 'date',
-                'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
-            );
-
-            // The Query
-            $events = new WP_Query($args);
-
-            // The Loop
-            if ($events->have_posts()) {
-                while ($events->have_posts()) {
-                    $events->the_post();
-                    get_template_part('components/cards/event-card');
-                } ?>
+    <!-- Start Body Section -->
+    <div class="bg-blue-gradient pb-5">
+    <div class="md:w-9/12 mx-auto grid grid-cols-12 p-5 gap-4 md:gap-10">
+        <div class="col-span-12 md:col-span-4">
+            <div class="bg-white">
+                <h3>Filter Events</h3>
+            </div>
+        </div>
 
 
-                <!-- Start Pagination-->
-                <div class="col-span-12 p-5 mb-8 pagination text-center">
+        <div class="col-span-12 md:col-span-8">
+            <div class="grid grid-cols-12 gap-4 md:gap-10">
+
+                <?php
+                // WP_Query arguments
+                $args = array(
+                    'post_type' => array('event'),
+                    'post_status' => array('publish'),
+                    'nopaging' => false,
+                    'order' => 'DESC',
+                    'orderby' => 'date',
+                    'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
+                );
+
+                // The Query
+                $events = new WP_Query($args);
+
+                // The Loop
+                if ($events->have_posts()) {
+                    while ($events->have_posts()) {
+                        $events->the_post();
+                        get_template_part('components/cards/event-card');
+                    }
+                } else { ?>
+                    <h3 class="text-center font-bold">There are no upcoming events.</h3>
                     <?php
-                    echo paginate_links(array(
-                        'base' => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-                        'total' => $events->max_num_pages,
-                        'current' => max(1, get_query_var('paged')),
-                        'format' => '?paged=%#%',
-                        'show_all' => false,
-                        'type' => 'list',
-                        'end_size' => 2,
-                        'mid_size' => 1,
-                        'prev_next' => true,
-                        'prev_text' => sprintf('<i></i> %1$s', __('Newer Videos', 'text-domain')),
-                        'next_text' => sprintf('%1$s <i></i>', __('Older Videos', 'text-domain')),
-                        'add_args' => false,
-                        'add_fragment' => '',
-                    ));
-                    ?>
-                </div>
-
-                <?php
-            } else { ?>
-                <h3 class="text-center font-bold">There are no upcoming events.</h3>
-                <?php
-            }
-            // Restore original Post Data
-            wp_reset_postdata();
-            ?>
-            <!-- End Pagination -->
+                }
+                // Restore original Post Data
+                wp_reset_postdata();
+                ?>
+            </div>
         </div>
     </div>
+
 
 
 <?php get_footer();
