a12a4f8df5470fe066c1cc1954cf21b41ab68290
Myzwer <josh@onefortyfivedesign.com>
2023-08-23 16:35:16 -0400

    :repeat: Refactored the fix I just made for Tailwind to need less updates in the future.

--- webpack/tailwind.config.js
+++ webpack/tailwind.config.js
@@ -8,12 +8,7 @@ module.exports = {
    *
    * More information: https://tailwindcss.com/blog/tailwindcss-v3
    */
-  content: [
-    "*.php",
-    "./components/cards/*.php",
-    "./components/headers/*.php",
-    "./assets/src/js/*.js",
-  ],
+  content: ["*.php", "./components/*/*.php", "./assets/src/js/*.js"],
   theme: {
     colors: {
       screens: {
