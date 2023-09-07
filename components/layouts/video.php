508e5ae463e2274c12191eb4542ab81f216c1374
Myzwer <josh@onefortyfivedesign.com>
2023-09-07 16:28:04 -0400

    :bug: Fixed an issue with the video breaking its container on mobile. Also added a high and low title,

--- components/layouts/video.php
+++ components/layouts/video.php
@@ -10,7 +10,11 @@
 ?>
 
 <div class='mx-auto grid grid-cols-12'>
-    <div class="col-span-12">
-        <?php the_sub_field("video_url"); ?>
+    <div class="col-span-12 lg:col-span-8 xl:col-span-6">
+        <h3 class="text-2xl font-bold mb-5"><?php the_sub_field("high_title"); ?></h3>
+        <div class="video-container">
+            <?php the_sub_field("video_url"); ?>
+        </div>
+        <h3 class="text-xl font-bold mt-5"><?php the_sub_field("low_title"); ?></h3>
     </div>
 </div>
