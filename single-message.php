c56e7689c99affe49677e3f2f47dec4d662b0a4e
Myzwer <josh@onefortyfivedesign.com>
2023-09-26 16:55:03 -0400

    :repeat: Refactored the single messages page to have more human readable code. Also moved popular messages into its own template partial so it can be reused across the site.

--- single-message.php
+++ single-message.php
@@ -11,8 +11,7 @@
 
 get_header(); ?>
 
-    <!-- start outer color -->
-    <!-- ******** THE FEATURED MESSAGE ******** -->
+    <!-- ******** THE MAIN MESSAGE ******** -->
     <div class="bg-blue-gradient">
         <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 pb-10 relative">
             <div class="col-span-12 pt-10">
@@ -20,64 +19,56 @@ get_header(); ?>
                     <?php the_field("youtube_link"); ?>
                 </div>
             </div>
+
             <div class="col-span-12">
+                <!-- Display Title -->
                 <h3 class="font-bold capitalize text-3xl pt-3"><?php the_title(); ?></h3>
 
-                <p class=" capitalize text-lg">
-                    <?php
-                    // DISPLAY SERIES NAME
-                    $taxonomy = 'speaker';
-                    // Get the terms associated with the current post
-                    $terms = get_the_terms(get_the_ID(), $taxonomy);
+                <!-- Function to display terms -->
+                <?php
+                function display_taxonomy_terms($taxonomy_name, $label)
+                {
+                    // Get the terms associated with the current post for the specified taxonomy
+                    $terms = get_the_terms(get_the_ID(), $taxonomy_name);
+
+                    // Check if there are terms and ensure no WP_Error occurred
                     if ($terms && !is_wp_error($terms)) {
-                        // Loop through the terms and display them
+
+                        // Initialize an array to store the term links
+                        $term_links = array();
+
+                        // Loop through each term
                         foreach ($terms as $term) {
-                            echo '<span class = "font-bold">Speaker:</span> <a href="' . get_term_link($term, $taxonomy) . '">' . esc_html($term->name) . '</a>';
+
+                            // Create a link to the term archive page
+                            $term_links[] = '<a href="' . get_term_link($term, $taxonomy_name) . '">' . esc_html($term->name) . '</a>';
                         }
+
+                        // Output a paragraph with the term label and links, separated by commas
+                        echo '<p class="capitalize text-lg"><span class="font-bold">' . esc_html($label) . ':</span> ' . implode(', ', $term_links) . '</p>';
                     }
-                    ?>
-                </p>
-                <p class="capitalize text-lg"><span class="font-bold">Date:</span> <?php echo get_the_date(); ?></p>
+                }
 
-                <p class=" capitalize text-lg">
-                    <?php
-                    // DISPLAY SERIES NAME
-                    $taxonomy = 'series';
-                    // Get the terms associated with the current post
-                    $terms = get_the_terms(get_the_ID(), $taxonomy);
-                    if ($terms && !is_wp_error($terms)) {
-                        // Loop through the terms and display them
-                        foreach ($terms as $term) {
-                            echo '<span class = "font-bold">Series:</span> <a href="' . get_term_link($term, $taxonomy) . '">' . esc_html($term->name) . '</a>';
-                        }
-                    }
-                    ?>
-                </p>
 
-                <p class="capitalize text-lg">
-                    <?php
-                    // DISPLAY SERIES NAME
-                    $taxonomy = 'topic';
+                // Display Speaker
+                display_taxonomy_terms('speaker', 'Speaker');
 
-                    // Get the terms associated with the current post
-                    $terms = get_the_terms(get_the_ID(), $taxonomy);
+                // Display Date
+                echo '<p class="capitalize text-lg"><span class="font-bold">Date:</span> ' . get_the_date() . '</p>';
 
-                    if ($terms && !is_wp_error($terms)) {
-                        // Extract term names into an array
-                        $term_names = wp_list_pluck($terms, 'name');
+                // Display Series
+                display_taxonomy_terms('series', 'Series');
 
-                        // Join the term names with commas
-                        $term_list = implode(', ', $term_names);
+                // Display Topic
+                display_taxonomy_terms('topic', 'Topics');
 
-                        echo '<span class="font-bold">Topics:</span> ' . esc_html($term_list);
-                    }
-                    ?>
-                </p>
-
-                <p class=" capitalize text-lg"><span class="font-bold">Notes:</span> <a href="#">View On Subsplash <i
-                                class="fa-regular fa-arrow-up-right-from-square"></i></a>
-                </p>
+                if (get_field('subsplash_link')): ?>
+                    <p class="capitalize text-lg"><span class="font-bold">Notes:</span> <a
+                                href="<?php the_field("subsplash_link") ?>">View On Subsplash <i
+                                    class="fa-regular fa-arrow-up-right-from-square"></i></a></p>
+                <?php endif; ?>
             </div>
+
         </div>
     </div>
 
@@ -88,14 +79,16 @@ get_header(); ?>
                 <p>All of our sermons are transcribed into blog format for you to read!</p>
                 <p>Blog format is typically available the following Wednesday.</p>
                 <div class="col-span-12 text-center">
-                    <button class="elevated-blue mt-3 mr-8">
-                        <a href="#">
-                            <i class="fa-solid fa-arrow-right"></i> Read Now
-                        </a>
-                    </button>
+                    <?php if (get_field('blog_link')): ?>
+                        <button class="elevated-blue mt-3 mr-8">
+                            <a href="<?php the_field("blog_link"); ?>">
+                                <i class="fa-solid fa-arrow-right"></i> Read Now
+                            </a>
+                        </button>
+                    <?php endif; ?>
 
                     <button class="ghost-paired mt-3">
-                        <a href="#" target="_blank">
+                        <a href="<?php the_field('blog_page', 'options'); ?>">
                             View All Blogs
                         </a>
                     </button>
@@ -103,89 +96,19 @@ get_header(); ?>
             </div>
         </div>
     </div>
+    <!--END MAIN MESSAGE -->
 
+
+    <!-- START POPULAR MESSAGES -->
     <div class="bg-blue-gradient">
         <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
-            <!-- "Popular Messages" text -->
-            <div class="col-span-12 text-left mt-10">
-                <h3 class="text-2xl md:text-3xl mb-3 font-bold">Popular Messages</h3>
-            </div>
-            <?php
-
-            // Check rows exists.
-            if (have_rows('popular_messages', 84)):
-
-                // Loop through rows.
-                while (have_rows('popular_messages', 84)) : the_row();
-                    ?>
-                    <div class="col-span-12 lg:col-span-4">
-
-                        <?php
-                        $featured_posts = get_sub_field('message');
-                        if ($featured_posts): ?>
-
-                            <?php foreach ($featured_posts as $post):
-
-                                // Setup this post for WP functions (variable must be named $post).
-                                setup_postdata($post); ?>
-
-                                <div class="relative">
-                                    <a href="<?php the_permalink(); ?>">
-                                        <img class="rounded-xl shadow-xl" src="<?php the_post_thumbnail_url(); ?>"
-                                             alt="Sermon Thumbnail">
-
-                                        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                            <div class="bg-black/[.5] rounded-full py-5 px-7">
-                                                <i class="fa-regular fa-play text-3xl text-white"></i>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                                <a href="<?php the_permalink(); ?>">
-                                    <h3 class="font-bold capitalize text-xl pt-3"><?php the_title(); ?></h3>
-
-                                    <p class=" capitalize text-lg">
-                                        <?php
-                                        // DISPLAY SERIES NAME
-                                        $taxonomy = 'series';
-                                        // Get the terms associated with the current post
-                                        $terms = get_the_terms(get_the_ID(), $taxonomy);
-                                        if ($terms && !is_wp_error($terms)) {
-                                            // Loop through the terms and display them
-                                            foreach ($terms as $term) {
-                                                echo 'Series: <a href="' . get_term_link($term, $taxonomy) . '">' . esc_html($term->name) . '</a>';
-                                            }
-                                        }
-                                        ?>
-                                    </p>
-                                    <p class=" capitalize text-sm"><?php the_date(); ?></p>
-                                </a>
-                            <?php endforeach; ?>
-
-                            <?php
-                            // Reset the global post object so that the rest of the page works correctly.
-                            wp_reset_postdata(); ?>
-                        <?php endif; ?>
-
-                    </div>
-                <?php
-
-                    // End loop.
-                endwhile;
-
-            endif;
-            ?>
-
-            <div class="col-span-12 text-center my-5">
-                <a href="<?php the_field("archive_link"); ?>">
-                    <button class="ghost mt-3">
-                        See All Messages
-                    </button>
-                </a>
-            </div>
+            <?php get_template_part('components/layouts/popular-messages'); ?>
         </div>
     </div>
+    <!-- END POPULAR MESSAGES -->
 
+
+    <!-- START APP CTA -->
     <div class="bg-white-gradient">
         <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
 
@@ -200,23 +123,21 @@ get_header(); ?>
                         reprehenderit ullam vero vitae voluptatum? A dolores error, facilis harum quis reprehenderit,
                         sapiente tempore vel velit vero voluptas!</p>
 
-                    <a href="<?php the_field("archive_link"); ?>">
+                    <a href="https://apps.apple.com/us/app/foothills-church/id520919170" target="_blank">
                         <button class="ghost mt-3 mr-8">
                             Download IOS
                         </button>
                     </a>
-                    <a href="<?php the_field("archive_link"); ?>">
+                    <a href="https://play.google.com/store/apps/details?id=com.subsplash.thechurchapp.foothillschurch&pli=1"
+                       target="_blank">
                         <button class="ghost mt-3">
                             Download Android
                         </button>
                     </a>
                 </div>
             </div>
-
+            <!-- END POPULAR MESSAGES -->
         </div>
-    </div>
-    </div>
-
     </div>
 
 
