26244c47b0bf682e0bf84d761d34dcd2d7adb43b
Myzwer <josh@onefortyfivedesign.com>
2023-10-03 13:06:46 -0400

    :sparkle: Made the pages dynamic using ACF

--- components/cards/location-card.php
+++ components/cards/location-card.php
@@ -2,15 +2,19 @@
 
 <div class="mt-5">
     <p class="uppercase text-sm font-bold">Join us at our</p>
-    <h2 class="capitalize font-bold text-2xl">Maryville Location</h2>
-    <p>1551 W Lamar Alexander Pkwy <br/> Maryville TN, 37801</p>
+    <h2 class="capitalize font-bold text-2xl"><?php the_sub_field("location_title"); ?></h2>
+    <p><?php the_sub_field("address"); ?></p>
 </div>
 
 <div class="mb-5">
-    <button class="elevated-blue mt-3 mr-3">
-        <i class="fa-solid fa-arrow-right"></i> Plan A Visit
-    </button>
-    <button class="ghost-paired mt-3">
-        Launch Maps
-    </button>
+    <a href="<?php the_sub_field("location_page"); ?>">
+        <button class="elevated-blue mt-3 mr-3">
+            <i class="fa-solid fa-arrow-right"></i> Plan A Visit
+        </button>
+    </a>
+    <a href="<?php the_sub_field("maps_link"); ?>" target="_blank">
+        <button class="ghost-paired mt-3">
+            Launch Maps
+        </button>
+    </a>
 </div>
--- find-location.php
+++ find-location.php
@@ -17,21 +17,33 @@ get_header(); ?>
 
                 <div class="grid grid-cols-12">
                     <div class="col-span-12 pb-5">
-                        <h3 class="uppercase font-bold">one church. two locations.</h3>
+                        <h3 class="uppercase font-bold"><?php the_field("tagline"); ?></h3>
                     </div>
 
-                    <div class="col-span-12 pt-5">
-                        <?php get_template_part('components/cards/location-card'); ?>
-                    </div>
 
-                    <div class="col-span-12 pt-5">
-                        <?php get_template_part('components/cards/location-card'); ?>
-                    </div>
+                    <?php
+
+                    // Check rows existexists.
+                    if (have_rows('location')):
+                        while (have_rows('location')) : the_row();
+                            ?>
+                            <div class="col-span-12 pt-5">
+                                <?php get_template_part('components/cards/location-card'); ?>
+                            </div>
+                        <?php
+                        endwhile;
+                    endif;
+                    ?>
+
+
                 </div>
             </div>
 
             <div class="col-span-12 md:col-span-6 lg:col-span-8 relative overflow-hidden">
-                <?php echo do_shortcode('[wpgmza id="1"]'); ?>
+                <?php
+                $maps = get_field('maps_shortcode');
+                echo do_shortcode($maps);
+                ?>
             </div>
         </div>
     </div>
