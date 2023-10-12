39c4a94c8fb93cf5d074d9606eefb7de7791b298
Myzwer <josh@onefortyfivedesign.com>
2023-10-12 16:37:29 -0400

    :rocket: Added a shortcode for the main sytle of button

--- functions.php
+++ functions.php
@@ -422,9 +422,25 @@ flush_rewrite_rules(false);
 //*****************************************************
 
 //******************** BUTTONS *********************
+
+/*
+ * FAB MAIN BUTTON
+ * Defaults to "#" if no value is given
+ * Usage:  [main_button text="Learn More" url="https://example.com"]
+*/
+
+function fab_main_button_shortcode($atts, $content = null)
+{
+    $button_text = isset($atts['text']) ? $atts['text'] : 'Learn More';
+    $button_url = isset($atts['url']) ? $atts['url'] : '#';
+    return '<a href="' . esc_url($button_url) . '"><button class="fab-main mt-3">' . esc_html($button_text) . '</button></a>';
+}
+
+add_shortcode('main_button', 'fab_main_button_shortcode');
+
 /*
  * GHOST BUTTON
- * Defaults to "#" if no vallue is given
+ * Defaults to "#" if no value is given
  * Usage:  [ghost_button text="Learn More" url="https://example.com"]
 */
 
@@ -449,7 +465,7 @@ function facebook_shortcode($atts, $content = null)
 {
     $button_size = isset($atts['size']) ? $atts['size'] : 2;
     $button_url = isset($atts['url']) ? $atts['url'] : 'https://www.facebook.com/foothillschurchTN';
-    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-brands fa-facebook"></i></a>';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . esc_html($button_size) . 'xl pr-1 fa-brands fa-facebook"></i></a>';
 }
 
 add_shortcode('facebook', 'facebook_shortcode');
@@ -464,7 +480,7 @@ function instagram_shortcode($atts, $content = null)
 {
     $button_size = isset($atts['size']) ? $atts['size'] : 2;
     $button_url = isset($atts['url']) ? $atts['url'] : 'https://www.instagram.com/foothillschurchtn/';
-    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-brands fa-instagram"></i></a>';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . esc_html($button_size) . 'xl pr-1 fa-brands fa-instagram"></i></a>';
 }
 
 add_shortcode('instagram', 'instagram_shortcode');
@@ -479,7 +495,7 @@ function x_shortcode($atts, $content = null)
 {
     $button_size = isset($atts['size']) ? $atts['size'] : 2;
     $button_url = isset($atts['url']) ? $atts['url'] : 'https://twitter.com/foothillschurch';
-    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-brands fa-x-twitter"></i></a>';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . esc_html($button_size) . 'xl pr-1 fa-brands fa-x-twitter"></i></a>';
 }
 
 add_shortcode('x', 'x_shortcode');
@@ -494,7 +510,7 @@ function website_shortcode($atts, $content = null)
 {
     $button_size = isset($atts['size']) ? $atts['size'] : 2;
     $button_url = isset($atts['url']) ? $atts['url'] : 'https://foothillschurch.com/';
-    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-solid fa-link-simple"></i></a>';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . esc_html($button_size) . 'xl pr-1 fa-solid fa-link-simple"></i></a>';
 }
 
 add_shortcode('website', 'website_shortcode');
