b6121c9dcadfa5c223a240d096e9a98e41cbdec7
Myzwer <josh@onefortyfivedesign.com>
2023-10-12 13:40:47 -0400

    :rocket: Added shortcodes to generate socials from a wysiwyg editor.

--- functions.php
+++ functions.php
@@ -421,8 +421,13 @@ flush_rewrite_rules(false);
 //******************** SHORTCODES *********************
 //*****************************************************
 
-// Usage:
-// [ghost_button text="Learn More" url="https://example.com"]
+//******************** BUTTONS *********************
+/*
+ * GHOST BUTTON
+ * Defaults to "#" if no vallue is given
+ * Usage:  [ghost_button text="Learn More" url="https://example.com"]
+*/
+
 function custom_button_shortcode($atts, $content = null)
 {
     $button_text = isset($atts['text']) ? $atts['text'] : 'Learn More';
@@ -432,5 +437,68 @@ function custom_button_shortcode($atts, $content = null)
 
 add_shortcode('ghost_button', 'custom_button_shortcode');
 
+
+//******************** SOCIALS *********************
+/*
+ * FACEBOOK ICON
+ * Defaults to the FC Facebook if no url is given
+ * You can feed it any size between 1 and 10. Defaults to 2.
+ * Usage:  [facebook url = "https://example.com" size = "1-10"]
+*/
+function facebook_shortcode($atts, $content = null)
+{
+    $button_size = isset($atts['size']) ? $atts['size'] : 2;
+    $button_url = isset($atts['url']) ? $atts['url'] : 'https://www.facebook.com/foothillschurchTN';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-brands fa-facebook"></i></a>';
+}
+
+add_shortcode('facebook', 'facebook_shortcode');
+
+/*
+ * INSTAGRAM ICON
+ * Defaults to the FC Instagram if no url is given
+ * You can feed it any size between 1 and 10. Defaults to 2.
+ * Usage:  [instagram url = "https://example.com" size = "1-10"]
+*/
+function instagram_shortcode($atts, $content = null)
+{
+    $button_size = isset($atts['size']) ? $atts['size'] : 2;
+    $button_url = isset($atts['url']) ? $atts['url'] : 'https://www.instagram.com/foothillschurchtn/';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-brands fa-instagram"></i></a>';
+}
+
+add_shortcode('instagram', 'instagram_shortcode');
+
+/*
+ * X ICON
+ * Defaults to the FC X account if no url is given
+ * You can feed it any size between 1 and 10. Defaults to 2.
+ * Usage:  [x url = "https://example.com" size = "1-10"]
+*/
+function x_shortcode($atts, $content = null)
+{
+    $button_size = isset($atts['size']) ? $atts['size'] : 2;
+    $button_url = isset($atts['url']) ? $atts['url'] : 'https://twitter.com/foothillschurch';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-brands fa-x-twitter"></i></a>';
+}
+
+add_shortcode('x', 'x_shortcode');
+
+/*
+ * WEBSITE ICON
+ * Defaults to the fc site if no url is given
+ * You can feed it any size between 1 and 10. Defaults to 2.
+ * Usage:  [website url = "https://example.com" size = "1-10"]
+*/
+function website_shortcode($atts, $content = null)
+{
+    $button_size = isset($atts['size']) ? $atts['size'] : 2;
+    $button_url = isset($atts['url']) ? $atts['url'] : 'https://foothillschurch.com/';
+    return '<a href="' . esc_url($button_url) . '"><i class="text-' . $button_size . 'xl pr-1 fa-solid fa-link-simple"></i></a>';
+}
+
+add_shortcode('website', 'website_shortcode');
+
+// Allows WP to inject shortcodes via a wysiwyg editor
 add_filter('widget_text', 'do_shortcode');
 add_filter('the_content', 'do_shortcode');
