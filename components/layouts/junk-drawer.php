95720585a001d96813df528f4c7f635803206c45
Myzwer <josh@onefortyfivedesign.com>
2023-10-18 16:36:36 -0400

    :white_check_mark: Added the junk drawer template partial. It contains a repeater that allows for chunks of small copy and a single button, repeated on a grid as many times as needed.

---
+++ components/layouts/junk-drawer.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Junk Drawer Template Partial
+ *
+ * REQUIRED ACF FIELDS:
+ *  - junk_drawer (repeater)
+ *     - Title (text)
+ *     - Paragraph (wysiwyg Editor)
+ *     - Button Text (text)
+ *     - Button Link (link)
+ */
+?>
+
+<!-- Start an outer grid. This can be nested inside of a different grid, but it'll look jank. -->
+<div class="grid grid-cols-12 gap-4 md:gap-10">
+    <?php
+    if (have_rows('junk_drawer')):
+        while (have_rows('junk_drawer')) : the_row(); ?>
+            <div class="col-span-12 md:col-span-4 p-5">
+                <h4 class="text-2xl pb-2 font-bold capitalize"><?php the_sub_field("title"); ?></h4>
+                <hr class="h-1 rounded-xl bg-black">
+                <div class="py-3 prose"><?php the_sub_field("paragraph"); ?></div>
+                <?php
+                // Hide the button if link is not provided.
+                if (get_sub_field('button_link')):
+                    ?>
+                    <a href="<?php the_sub_field("button_link"); ?>">
+                        <button class="ghost">
+                            <?php the_sub_field("button_text"); ?>
+                        </button>
+                    </a>
+                <?php endif; ?>
+            </div>
+        <?php endwhile;
+    endif; ?>
+
+</div>
