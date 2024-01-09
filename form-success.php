7e193cbcc7ec4f01095583662bbda9a6e4783aa9
Myzwer <josh@onefortyfivedesign.com>
2024-01-09 00:07:16 -0500

    :white_check_mark: Added the rest of the custom templates (I hope).

---
+++ form-success.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Form Submit Template
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Bootcamp_2
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <!-- Content goes here -->
+
+<?php get_footer();
---
+++ students.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Students Page
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Bootcamp_2
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <!-- Content goes here -->
+
+<?php get_footer();
