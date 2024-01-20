debe9698ab644d8ac78e37523776fc36860a598d
Myzwer <josh@onefortyfivedesign.com>
2024-01-20 15:21:49 -0500

    :rocket: Completed the gallery block template partial and linked it on page.php.

--- page.php
+++ page.php
@@ -101,6 +101,10 @@ if (have_rows('body_sections')) :
                 get_template_part('components/blocks/card');
                 break;
 
+            case 'gallery_block':
+                get_template_part('components/blocks/gallery');
+                break;
+
 
             // FIXME: Only for building/debugging, shouldn't be left in for production
             default:
