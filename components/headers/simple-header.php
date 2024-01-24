51befc637f0e3df7ef3b87b87159e9794d7ba621
Myzwer <josh@onefortyfivedesign.com>
2024-01-24 00:12:58 -0500

    :scroll: Added a subtitle acf field

--- components/headers/simple-header.php
+++ components/headers/simple-header.php
@@ -2,7 +2,7 @@
 /*
  * REQUIRED ACF FIELDS:
  * simple_title (text field)
- *
+ * simple_subtitle (text field, kinda optional)
  * */
 ?>
 
@@ -12,6 +12,9 @@
                  url('<?php echo get_template_directory_uri(); ?>/assets/src/img/topography.png') center center;
                  height: 20vh;">
         <div class="content-middle text-center">
+            <h2 class="text-white text-xl md:text-2xl font-bold uppercase"><?php
+                the_field('simple_subtitle');
+                ?></h2>
             <h1 class="text-white text-3xl md:text-5xl font-bold uppercase"><?php
                 the_field('simple_title');
                 ?></h1>
