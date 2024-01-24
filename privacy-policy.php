a8bfbfd46bfacfda190986a070f56444cf78eec3
Myzwer <josh@onefortyfivedesign.com>
2024-01-24 00:58:05 -0500

    :rocket: Finished the privacy Policy page. Added all the ACF fields needed as well.

--- privacy-policy.php
+++ privacy-policy.php
@@ -13,20 +13,19 @@
 
 get_header(); ?>
 
-    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
-  rgba(0, 0, 0, 0.45),
-  rgba(0, 0, 0, 0.45)
-), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
- height: 60vh;">
-        <div class="content-middle text-white text-center">
-            <h1 class="text-4xl mb-5">Privacy Policy</h1>
-        </div>
-    </div>
+    <!-- Simple Header-->
+<?php get_template_part('components/headers/simple-header'); ?>
+    <!-- End Header -->
 
 
-    <div class="m-4 md:m-10 lg:max-w-4xl lg:mx-auto privacy-policy">
+    <div class="m-4 md:m-10 lg:max-w-6xl lg:mx-auto privacy-policy">
         <div class="grid grid-cols-12 gap-4">
-            <div class="col-span-12 p-5">
+            <div class="col-span-12 p-5 prose">
+                <img class="rounded-xl shadow-xl" src="<?php the_field('banner_image'); ?>"
+                     alt="Privacy Policy Banner Image">
+            </div>
+
+            <div class="col-span-12 p-5 prose">
                 <?php if (have_posts()) : while (have_posts()) : the_post();
                     the_content();
                 endwhile;
