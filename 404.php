22e06b6dd50f20e75d2723f527ad6a07923fcd95
Myzwer <josh@onefortyfivedesign.com>
2024-01-31 13:38:14 -0500

    :rocket: Completed the 404 Page design per the mockup.

--- 404.php
+++ 404.php
@@ -13,30 +13,24 @@
 
 get_header(); ?>
 
-    <div class="grid grid-cols-12">
-        <div class="col-span-12 md:col-span-6 text-center">
-            <div class="md:text-left md:mx-10">
-                <div class="w-20 mt-11">
-                    <img src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png"
-                         alt="Company Logo">
-                </div>
+    <div class="bg-blue-gradient relative full-background-lg">
+        <div class="bg-no-repeat bg-scroll bg-cover  full-background-lg relative"
+             style="background:
+                     url('<?php echo get_template_directory_uri(); ?>/assets/src/img/topography-salty.png') center center; ">
 
-                <p class="md:mt-10">404 Error</p>
-                <h1 class="uppercase text-2xl md:text-3xl md:mb-10">Page Not Found</h1>
-                <p>The rocket took off without this page ¯\_(ツ)_/¯</p>
-                <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                    Back to Homepage
-                </button>
+            <div class="text-center content-middle-medium mx-auto py-20 relative">
+                <img class="block mx-auto w-1/2 md:w-fit pt-5" src="<?php the_field('tomb', 'options'); ?>"
+                     alt="404 Tomb Picture">
+                <h1 class="uppercase text-3xl md:text-6xl font-bold"><?php the_field('title', 'options'); ?></h1>
+                <h3 class="uppercase text-2xl font-bold pt-3"><?php the_field('subtitle', 'options'); ?></h3>
+                <p class="text-xl font-bold pt-5"><?php the_field('explination', 'options'); ?></p>
+                <a class="pt-5" href="<?php the_field("button_link", 'options'); ?>">
+                    <button class="fab-main mt-3">
+                        <i class="fa-solid fa-circle-arrow-right"></i> <?php the_field("button_text", 'options'); ?>
+                    </button>
+                </a>
             </div>
-        </div>
 
-        <div class="col-span-12 md:col-span-6">
-            <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
-				  rgba(0, 0, 0, 0.45),
-				  rgba(0, 0, 0, 0.45)
-				), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
-				 height: 80vh;">
-            </div>
         </div>
     </div>
 
