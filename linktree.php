db818a5626b49464a054beac0ca056703531355d
Myzwer <josh@onefortyfivedesign.com>
2024-03-11 14:26:17 -0400

    :rocket: Completed the linktree page, added fallbacks if no header code is provided, styled the buttons.

--- linktree.php
+++ linktree.php
@@ -29,7 +29,7 @@ $socials = ( get_field( 'socials' ) == 'no' ) ? 'hidden' : 'block';
 ?>
 
     <div class="bg-blue-gradient relative">
-        <div class="bg-no-repeat bg-scroll bg-cover relative"
+        <div class="bg-no-repeat bg-scroll bg-cover relative pb-20"
              style="background:
                      url('<?php echo get_template_directory_uri(); ?>/assets/src/img/topography-salty.png') center center; ">
 
@@ -64,77 +64,83 @@ $socials = ( get_field( 'socials' ) == 'no' ) ? 'hidden' : 'block';
                 </div>
             </div>
 
+			<?php
+			if ( have_rows( 'link_group' ) ):
+				while ( have_rows( 'link_group' ) ) :
+					the_row();
+					?>
+                    <div class="lg:max-w-3xl mx-auto grid grid-cols-12 p-5 mt-2">
+                        <div class="col-span-12 text-center">
+                            <h3 class="text-xl font-bold"><?php the_sub_field( 'section_title' ); ?></h3>
+                        </div>
 
-            <div class="lg:max-w-3xl mx-auto grid grid-cols-12 p-5 mt-2">
-                <div class="col-span-12 text-center">
-                    <h3 class="text-xl font-bold">Locations</h3>
-                </div>
+						<?php
+						if ( have_rows( 'link' ) ):
+							while ( have_rows( 'link' ) ) : the_row(); ?>
 
-                <div class="col-span-12">
-                    <a class="block w-full" href="#">
-                        <button class="elevated-white-lt mt-3 relative">
-                            <div class="absolute left-5">
-                                <i class="fa-solid fa-circle-arrow-right"></i>
-                            </div>
-                            Button text
-                        </button>
-                    </a>
-                </div>
-                <div class="col-span-12">
-                    <a class="block w-full" href="#">
-                        <button class="elevated-white-lt mt-3 relative">
-                            <div class="absolute left-5">
-                                <i class="fa-solid fa-circle-arrow-right"></i>
-                            </div>
-                            Button text
-                        </button>
-                    </a>
-                </div>
-            </div>
 
-            <div class="lg:max-w-3xl mx-auto grid grid-cols-12 p-5 mt-2">
-                <div class="col-span-12 text-center">
-                    <h3 class="text-xl font-bold">Events</h3>
-                </div>
+								<?php if ( get_sub_field( 'hide_button' ) == 'no' & get_sub_field( 'prioritize' ) == 'no' ) { ?>
+                                    <div class="col-span-12">
+                                        <a class="block w-full" href="<?php the_sub_field( 'button_link' ); ?>">
+                                            <button class="elevated-white-lt mt-3 relative">
+                                                <div class="absolute left-5">
+													<?php the_sub_field( 'icon' ); ?>
+                                                </div>
+												<?php the_sub_field( 'button_text' ); ?>
+                                            </button>
+                                        </a>
+                                    </div>
+								<?php } ?>
 
-                <div class="col-span-12 relative mt-5 ">
-                    <a class="block w-full lt-image" href="#">
-                        <img src="https://placehold.co/1920x1080" alt="">
-                        <h3 class="capitalize font-bold text-xl text-black py-3 text-center">button text</h3>
-                    </a>
-                </div>
-                
-                <div class="col-span-12">
-                    <a class="block w-full" href="#">
-                        <button class="elevated-white-lt mt-3 relative">
-                            <div class="absolute left-5">
-                                <i class="fa-solid fa-circle-arrow-right"></i>
-                            </div>
-                            Button text
-                        </button>
-                    </a>
-                </div>
-                <div class="col-span-12">
-                    <a class="block w-full" href="#">
-                        <button class="elevated-white-lt mt-3 relative">
-                            <div class="absolute left-5">
-                                <i class="fa-solid fa-circle-arrow-right"></i>
-                            </div>
-                            Button text
-                        </button>
-                    </a>
-                </div>
-                <div class="col-span-12">
-                    <a class="block w-full" href="#">
-                        <button class="elevated-white-lt mt-3 relative">
-                            <div class="absolute left-5">
-                                <i class="fa-solid fa-circle-arrow-right"></i>
-                            </div>
-                            Button text
-                        </button>
-                    </a>
-                </div>
-            </div>
+								<?php if ( get_sub_field( 'hide_button' ) == 'no' & get_sub_field( 'prioritize' ) == 'yes' ) { ?>
+                                    <div class="col-span-12 relative mt-5">
+                                        <a class="block w-full lt-image"
+                                           href="<?php the_sub_field( 'button_link' ); ?>">
+                                            <img class="lt-image-top" src="<?php the_sub_field( 'link_image' ); ?>"
+                                                 alt="Graphic">
+                                            <h3 class="capitalize font-bold text-xl text-black py-3 text-center">
+												<?php the_sub_field( 'button_text' ); ?>
+                                            </h3>
+                                        </a>
+                                    </div>
+								<?php } ?>
+
+							<?php
+							endwhile;
+						endif;
+						?>
+
+                    </div>
+				<?php
+				endwhile;
+			endif;
+			?>
+
+
+            <!-- <div class="lg:max-w-3xl mx-auto grid grid-cols-12 p-5 mt-2">
+				 <div class="col-span-12 text-center">
+					 <h3 class="text-xl font-bold">Events</h3>
+				 </div>
+
+				 <div class="col-span-12 relative mt-5 ">
+					 <a class="block w-full lt-image" href="#">
+						 <img src="https://placehold.co/1920x1080" alt="">
+						 <h3 class="capitalize font-bold text-xl text-black py-3 text-center">button text</h3>
+					 </a>
+				 </div>
+
+				 <div class="col-span-12">
+					 <a class="block w-full" href="#">
+						 <button class="elevated-white-lt mt-3 relative">
+							 <div class="absolute left-5">
+								 <i class="fa-solid fa-circle-arrow-right"></i>
+							 </div>
+							 Button text
+						 </button>
+					 </a>
+				 </div>
+			 </div>-->
+
 
         </div>
     </div>
