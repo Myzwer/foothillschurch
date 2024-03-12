4051ee30d8783bcd9bde163d8dfb52183db32186
Myzwer <josh@onefortyfivedesign.com>
2024-03-12 14:00:29 -0400

    :sparkle: Switched a few of the items on this page to be global pulls from the ACF options page instead of local to this page.

--- app.php
+++ app.php
@@ -19,7 +19,7 @@ get_header(); ?>
 
             <div class="lg:max-w-8xl mx-auto grid grid-cols-12 p-5 py-10 gap-4">
                 <div class="col-span-12 lg:col-span-5 md:col-start-1">
-                    <img class="block mx-auto  md:w-fit pt-5" src="<?php the_field( 'app' ); ?>"
+                    <img class="block mx-auto  md:w-fit pt-5" src="<?php the_field( 'app_promo', 'options' ); ?>"
                          alt="App on Phone">
                 </div>
 
@@ -30,13 +30,13 @@ get_header(); ?>
                         </h1>
                         <p class=""><?php the_field( 'details' ); ?></p>
                         <div class="pt-5">
-                            <a class="pr-2" href="<?php the_field( "app_store_link_apple" ); ?>" target="_blank">
+                            <a class="pr-2" href="<?php the_field( "app_store_link", 'options' ); ?>" target="_blank">
                                 <button class="ghost-black">
                                     <i class="fa-brands fa-apple"></i> App Store
                                 </button>
                             </a>
 
-                            <a href="<?php the_field( "play_store_link" ); ?>" target="_blank">
+                            <a href="<?php the_field( "play_store_link", 'options' ); ?>" target="_blank">
                                 <button class="ghost-black">
                                     <i class="fa-brands fa-google-play"></i> Play Store
                                 </button>
