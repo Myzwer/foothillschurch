d828ac3b172a83ae48453383434334b9a97683b4
Myzwer <josh@onefortyfivedesign.com>
2024-03-21 16:03:35 -0400

    :rocket: Added a top "subtitle" line to the header

--- components/headers/default/_simple.php
+++ components/headers/default/_simple.php
@@ -12,9 +12,10 @@
                  url('<?php echo get_template_directory_uri(); ?>/assets/src/img/topography.png') center center;
                  height: 20vh;">
         <div class="content-middle text-center">
-            <h1 class="text-white text-3xl md:text-5xl font-bold uppercase"><?php
-                the_sub_field('simple_title');
-                ?></h1>
+            <div class="center add-padding">
+                <h2 class="text-white text-xl md:text-2xl lb-2 font-bold"><?php the_sub_field( "small_subtitle" ); ?></h2>
+            </div>
+            <h1 class="text-white text-3xl md:text-5xl uppercase font-bold"><?php the_sub_field( "main_title" ); ?></h1>
         </div>
     </div>
 </div>
