7929aa5933ccd0f6eb0b5c1789b831d9615a257e
Myzwer <josh@onefortyfivedesign.com>
2024-03-21 10:12:51 -0400

    :bug: Fixed an issue where the button classes got changed in a previous commit, and they never got updated in these files.

--- components/layouts/popular-messages.php
+++ components/layouts/popular-messages.php
@@ -5,21 +5,21 @@
 <?php
 
 // Check rows exists.
-if (have_rows('popular_messages', 84)):
+if ( have_rows( 'popular_messages', 84 ) ):
 
-    // Loop through rows.
-    while (have_rows('popular_messages', 84)) : the_row();
-        ?>
+	// Loop through rows.
+	while ( have_rows( 'popular_messages', 84 ) ) : the_row();
+		?>
         <div class="col-span-12 lg:col-span-4">
 
-            <?php
-            $featured_posts = get_sub_field('message');
-            if ($featured_posts): ?>
+			<?php
+			$featured_posts = get_sub_field( 'message' );
+			if ( $featured_posts ): ?>
 
-                <?php foreach ($featured_posts as $post):
+				<?php foreach ( $featured_posts as $post ):
 
-                    // Setup this post for WP functions (variable must be named $post).
-                    setup_postdata($post); ?>
+					// Setup this post for WP functions (variable must be named $post).
+					setup_postdata( $post ); ?>
 
                     <div class="relative">
                         <a href="<?php the_permalink(); ?>">
@@ -37,40 +37,40 @@ if (have_rows('popular_messages', 84)):
                         <h3 class="font-bold capitalize text-xl pt-3"><?php the_title(); ?></h3>
 
                         <p class=" capitalize text-lg">
-                            <?php
-                            // DISPLAY SERIES NAME
-                            $taxonomy = 'series';
-                            // Get the terms associated with the current post
-                            $terms = get_the_terms(get_the_ID(), $taxonomy);
-                            if ($terms && !is_wp_error($terms)) {
-                                // Loop through the terms and display them
-                                foreach ($terms as $term) {
-                                    echo 'Series: <a href="' . get_term_link($term, $taxonomy) . '">' . esc_html($term->name) . '</a>';
-                                }
-                            }
-                            ?>
+							<?php
+							// DISPLAY SERIES NAME
+							$taxonomy = 'series';
+							// Get the terms associated with the current post
+							$terms = get_the_terms( get_the_ID(), $taxonomy );
+							if ( $terms && ! is_wp_error( $terms ) ) {
+								// Loop through the terms and display them
+								foreach ( $terms as $term ) {
+									echo 'Series: <a href="' . get_term_link( $term, $taxonomy ) . '">' . esc_html( $term->name ) . '</a>';
+								}
+							}
+							?>
                         </p>
                         <p class=" capitalize text-sm"><?php the_date(); ?></p>
                     </a>
-                <?php endforeach; ?>
+				<?php endforeach; ?>
 
-                <?php
-                // Reset the global post object so that the rest of the page works correctly.
-                wp_reset_postdata(); ?>
-            <?php endif; ?>
+				<?php
+				// Reset the global post object so that the rest of the page works correctly.
+				wp_reset_postdata(); ?>
+			<?php endif; ?>
 
         </div>
-    <?php
+	<?php
 
-        // End loop.
-    endwhile;
+		// End loop.
+	endwhile;
 
 endif;
 ?>
 
 <div class="col-span-12 text-center my-5">
-    <a href="<?php the_field("archive_link"); ?>">
-        <button class="ghost mt-3">
+    <a href="<?php the_field( "archive_link" ); ?>">
+        <button class="ghost-black mt-3">
             See All Messages
         </button>
     </a>
--- messages.php
+++ messages.php
@@ -14,7 +14,7 @@
 get_header(); ?>
 
     <!-- Simple Header-->
-<?php get_template_part('components/headers/simple-header'); ?>
+<?php get_template_part( 'components/headers/simple-header' ); ?>
     <!-- End Header -->
 
     <!-- Start Body Section -->
@@ -29,23 +29,24 @@ get_header(); ?>
             </div>
 
             <!-- Start the actual card -->
-            <?php
-            //Drop into php to call the latest post title and link
-            $recent_posts = wp_get_recent_posts(array(
-                'post_type' => 'message',
-                'numberposts' => 1,
-                'post_status' => 'publish',
-            ));
+			<?php
+			//Drop into php to call the latest post title and link
+			$recent_posts = wp_get_recent_posts( array(
+				'post_type'   => 'message',
+				'numberposts' => 1,
+				'post_status' => 'publish',
+			) );
 
 
-            foreach (
-                $recent_posts
+			foreach (
+				$recent_posts
 
-                as $post) : ?>
+				as $post
+			) : ?>
                 <div class="col-span-12 text-center pt-3 relative">
-                    <a href="<?php echo get_permalink($post['ID']) ?>">
+                    <a href="<?php echo get_permalink( $post['ID'] ) ?>">
                         <img class="rounded-xl shadow-xl"
-                             src="<?php echo get_the_post_thumbnail_url($post['ID'], 'post-thumbnail'); ?>" alt="">
+                             src="<?php echo get_the_post_thumbnail_url( $post['ID'], 'post-thumbnail' ); ?>" alt="">
                         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                             <div class="bg-black/[.5] rounded-full py-8 px-10">
                                 <i class="fa-regular fa-play text-6xl text-white"></i>
@@ -57,30 +58,29 @@ get_header(); ?>
 
                 <div class="col-span-12 text-center my-8">
 
-                    <a href="<?php echo get_permalink($post['ID']) ?>">
+                    <a href="<?php echo get_permalink( $post['ID'] ) ?>">
                         <button class="elevated-blue mt-3 mr-3">
                             <i class="fa-solid fa-arrow-right"></i> Watch Now
                         </button>
                     </a>
 
-                    <a href="<?php the_field('youtube_link', $post['ID'], false, false); ?>" target="_blank">
+                    <a href="<?php the_field( 'youtube_link', $post['ID'], false, false ); ?>" target="_blank">
                         <button class="ghost-paired mt-3">
                             <!-- acf field, get the post ID of last post, "false false" strips formatting and provides raw URL -->
-
                             View on YouTube
                         </button>
                     </a>
 
                 </div>
-            <?php endforeach;
-            wp_reset_query(); ?>
+			<?php endforeach;
+			wp_reset_query(); ?>
             <!-- End Featured Sermon -->
         </div>
     </div>
 
     <div class="bg-blue-gradient">
         <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
-            <?php get_template_part('components/layouts/popular-messages'); ?>
+			<?php get_template_part( 'components/layouts/popular-messages' ); ?>
         </div>
     </div>
 
@@ -92,28 +92,28 @@ get_header(); ?>
                 <h3 class="text-2xl md:text-3xl mb-3 font-bold">Recent Messages</h3>
             </div>
 
-            <?php
-            // WP_Query arguments
-            $args = array(
-                'post_type' => array('message'),
-                'post_status' => array('publish'),
-                'posts_per_page' => 8,
-                'nopaging' => false,
-                'offset' => 1,
-                'order' => 'DESC',
-                'orderby' => 'date',
-                'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
-            );
+			<?php
+			// WP_Query arguments
+			$args = array(
+				'post_type'      => array( 'message' ),
+				'post_status'    => array( 'publish' ),
+				'posts_per_page' => 8,
+				'nopaging'       => false,
+				'offset'         => 1,
+				'order'          => 'DESC',
+				'orderby'        => 'date',
+				'paged'          => ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1,
+			);
 
-            // The Query
-            $recents = new WP_Query($args);
+			// The Query
+			$recents = new WP_Query( $args );
 
-            // The Loop
-            if ($recents->have_posts()) {
-                while ($recents->have_posts()) {
-                    $recents->the_post();
+			// The Loop
+			if ( $recents->have_posts() ) {
+				while ( $recents->have_posts() ) {
+					$recents->the_post();
 
-                    ?>
+					?>
 
                     <div class="col-span-6 lg:col-span-3 shadow-lg rounded-lg bg-blue-gradient height-lock-message">
                         <a href="<?php the_permalink(); ?>">
@@ -129,20 +129,20 @@ get_header(); ?>
                             </div>
                         </a>
                     </div>
-                <?php } ?>
+				<?php } ?>
 
 
-                <?php
-            } else {
-                echo 'there are no posts.'; // no posts found
-            }
-            // Restore original Post Data
-            wp_reset_postdata();
-            ?>
+				<?php
+			} else {
+				echo 'there are no posts.'; // no posts found
+			}
+			// Restore original Post Data
+			wp_reset_postdata();
+			?>
 
             <div class="col-span-12 text-center my-5">
-                <a href="<?php the_field("archive_link"); ?>">
-                    <button class="ghost mt-3">
+                <a href="<?php the_field( "archive_link" ); ?>">
+                    <button class="ghost-black mt-3">
                         See All Messages
                     </button>
                 </a>
