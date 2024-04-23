887748a3d18f6a08335820082cc95487cbbe70f2
Myzwer <josh@onefortyfivedesign.com>
2024-04-23 15:02:18 -0400

    :rocket: Added logic to handle which instagram account is used and display the correct feeds and links.

--- components/blocks/instagram.php
+++ components/blocks/instagram.php
@@ -16,12 +16,36 @@
  */
 ?>
 
+<?php $account = get_sub_field( 'instagram_account' );
+
+// Set proper feeds
+if ( $account === 'foothills' ) {
+	$feed1 = '1';
+	$feed2 = '2';
+} else if ( $account === 'students' ) {
+	$feed1 = '3';
+	$feed2 = '4';
+} else {
+	$feed1 = null;
+	$feed2 = null;
+}
+
+// Set link
+if ( $account === 'foothills' ) {
+	$link = "https://www.instagram.com/foothillschurchtn/?hl=en";
+} else if ( $account === 'students' ) {
+	$link = "https://www.instagram.com/fc_students/?hl=en";
+} else {
+	$link = null;
+}
+?>
+
+
 <div class="bg-no-repeat bg-scroll bg-cover relative pb-8"
      style="background: linear-gradient(
              rgba(245, 235, 232, 0.45),
              rgba(245, 235, 232, 0.45)
              ), url('<?php the_field( 'topography', 'option' ) ?>') center center;">
-
     <div class=" lg:max-w-6xl lg:text-center lg:mx-auto p-5 pt-10">
         <div class="grid grid-cols-12 gap-1">
             <div class="col-span-12 py-5 prose max-w-none">
@@ -29,7 +53,7 @@
             </div>
 
             <div class="col-span-12 prose max-w-none insta-margin-fix">
-				<?php echo do_shortcode( '[instagram-feed feed=1]' ); ?>
+				<?php echo do_shortcode( '[instagram-feed feed=' . $feed1 . ']' ); ?>
             </div>
 
             <div class="col-span-6 md:col-span-4 bg-white relative instagram">
@@ -38,7 +62,7 @@
                         <div class="absolute bottom-2 px-5 pb-3">
                             <h2 class="text-xl md:text-3xl font-bold uppercase text-left md:pb-2"><?php the_sub_field( "title" ); ?></h2>
                             <div class="text-left">
-                                <a href="<?php the_sub_field( "button_link" ); ?>" class="text-left">
+                                <a href="<?php echo $link; ?>" class="text-left" target="_blank">
                                     <button class="gallery-ghost text-left">
                                         <i class="fa-solid fa-arrow-right"></i> <?php the_sub_field( "button_text" ); ?>
                                     </button>
@@ -50,7 +74,7 @@
             </div>
 
             <div class="col-span-6 md:col-span-8 prose max-w-none">
-				<?php echo do_shortcode( '[instagram-feed feed=2]' ); ?>
+				<?php echo do_shortcode( '[instagram-feed feed=' . $feed2 . ']' ); ?>
             </div>
 
         </div>
