748978f4a9cc01fe09c7f7969e5574a3d034153d
Myzwer <josh@onefortyfivedesign.com>
2024-04-23 15:01:05 -0400

    :repeat: Complete refactor of this page to be more versatile for content editors. Now uses some static content on the top then allows for flex content on the bottom half of the page for further customization.

--- location.php
+++ location.php
@@ -11,147 +11,141 @@
  * @since 1.0.0
  */
 
+
 get_header(); ?>
 
     <video class="header-video"
-           src="<?php the_field("background_video"); ?>" autoplay loop
+           src="<?php the_field( "background_video" ); ?>" autoplay loop
            playsinline muted></video>
 
     <div class="viewport-header">
         <div class="head-container">
             <div class="center add-padding">
-                <h2 class="text-white text-xl md:text-3xl lb-2 font-bold"><?php the_field("top_line"); ?></h2>
+
+				<?php
+				$header_choice = get_field( 'header_style' );
+
+				if ( $header_choice == "Logo" ): ?>
+                    <img class="w-3/4 md:w-1/2 mx-auto" src="<?php the_field( 'brand' ); ?>" alt="Branding">
+                    <h1 class="text-white text-3xl  uppercase font-bold">
+						<?php the_field( "location_title" ); ?>
+                    </h1>
+				<?php endif; ?>
+
+				<?php if ( $header_choice == "Text" ): ?>
+                    <div class="center add-padding">
+                        <h2 class="text-white text-xl md:text-3xl lb-2 font-bold"><?php the_field( "top_line" ); ?></h2>
+                        <h1 class="text-white text-5xl  uppercase font-bold">
+							<?php the_field( "location_title" ); ?>
+                        </h1>
+                    </div>
+				<?php endif; ?>
             </div>
-            <h1 class="text-white text-3xl md:text-5xl uppercase font-bold">
-                <?php the_field("location_title"); //see note in doc block ?>
-            </h1>
         </div>
     </div>
 
+<?php
+if ( have_rows( 'announcement_banner' ) ):
+	while ( have_rows( 'announcement_banner' ) ): the_row(); ?>
+        <div class="bg-salty-gradient">
+            <div class="xl:w-8/12 max-w-screen-2xl mx-auto p-5 xl:p-5">
+                <div class="grid grid-cols-12 gap-4 md:gap-4">
+                    <div class="col-span-12 py-5 prose max-w-none announcement-block">
+						<?php the_sub_field( 'announcement_content' ); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+	<?php
+	endwhile;
+endif;
+?>
+
+    <!-- Location Information. This is the contact info, location pastor, and schedule. -->
     <div class="bg-white-gradient pb-10">
-        <div class="lg:max-w-5xl lg:mx-auto p-5 pt-10">
-            <div class="grid grid-cols-12 gap-4 md:gap-10">
-
-                <div class="col-span-12 md:col-span-6">
-                    <hr class="h-1 rounded-xl bg-black">
-                    <h2 class="text-2xl py-2 font-bold capitalize"><?php the_field("info_block_title"); ?></h2>
-                    <?php
-                    // Check rows exists.
-                    if (have_rows('info_block')):
-                        while (have_rows('info_block')) : the_row();
-                            ?>
-                            <div class="block pb-4">
-                                <h3 class="capitalize text-xl font-bold"><?php the_sub_field("subtitle"); ?></h3>
-                                <p class="prose"><?php the_sub_field("subtext"); ?></p>
-                            </div>
-                        <?php
-                        endwhile;
-                    endif; ?>
-                </div>
-
-
-                <div class="col-span-12 md:col-span-6">
-                    <hr class="h-1 rounded-xl bg-black">
-                    <h2 class="text-2xl py-2 font-bold capitalize"><?php the_field("pastor_title"); ?></h2>
-                    <div class="grid grid-cols-12 gap-4 md:gap-10">
-                        <div class="col-span-12 md:col-span-4">
-                            <img class="rounded-xl shadow-xl" src="<?php the_field("headshot"); ?>" alt="">
-                        </div>
-                        <div class="col-span-12 md:col-span-8">
-                            <h3 class="capitalize text-xl font-bold"><?php the_field("pastor_name"); ?></h3>
-                            <div class="prose"><?php the_field("pastor_bio"); ?></div>
-                        </div>
-
-
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6">
-                    <hr class="h-1 rounded-xl bg-black">
-                    <h2 class="text-2xl py-2 font-bold capitalize"><?php the_field("info_block_title_2"); ?></h2>
-
-                    <?php
-                    // Check rows exists.
-                    if (have_rows('info_block_2')):
-                        while (have_rows('info_block_2')) : the_row();
-                            ?>
-                            <div class="block pb-4">
-                                <h3 class="capitalize text-xl font-bold"><?php the_sub_field("subtitle"); ?></h3>
-                                <p class="prose"><?php the_sub_field("subtext"); ?></p>
-                            </div>
-                        <?php
-                        endwhile;
-                    endif; ?>
-                </div>
-            </div>
-        </div>
+		<?php get_template_part( 'components/layouts/location-info' ); ?>
     </div>
 
-    <!-- Start Body Section -->
-    <div class="bg-blue-gradient">
-        <div class="xl:w-9/12 max-w-screen-2xl mx-auto pt-5 xl:p-5">
-            <div class="grid grid-cols-12 gap-4 md:gap-4">
-                <div class="col-span-12">
-                    <h2 class="text-2xl py-4 font-bold capitalize text-center">Upcoming Events
-                        in <?php the_field("location_title"); ?></h2>
-                </div>
 
-                <?php
-                $location = get_field("location_title");
-                // WP_Query arguments
-                $args = array(
-                    'post_type' => array('event'),
-                    'post_status' => array('publish'),
-                    'nopaging' => false,
-                    'order' => 'DESC',
-                    'orderby' => 'date',
-                    'posts_per_page' => 3, // Use 'posts_per_page' instead of 'numberposts'
-                    'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
-                    'tax_query' => array(
-                        array(
-                            'taxonomy' => 'event_location',
-                            'field' => 'slug', // You can use 'slug' or 'term_id' based on your needs
-                            'terms' => $location, // Use the term ID of the current term
-                        ),
-                    ),
-                );
+    <!--
+	*
+	*
+	*
+	*
+	* FLEX CONTENT
+	* Load the rest of the content in via flex content so its more customizable.
+	*
+	*
+	*
+	*
+	-->
+<?php
+// Check value exists.
+if ( have_rows( 'other_sections' ) ) :
 
-                // The Query
-                $events = new WP_Query($args);
+	// used for alternating background colors
+	$counter = 0;
 
-                // The Loop
-                if ($events->have_posts()) {
-                    while ($events->have_posts()) {
-                        $events->the_post();
-                        $size_select = array(
-                            'column_span_class' => 'lg:col-span-4'
-                        );
-                        get_template_part('components/cards/event-card', null, $size_select);
-                    }
-                } else { ?>
-                    <div class="col-span-12">
-                        <h3 class="text-center font-bold">This location has no upcoming events.</h3>
-                    </div>
-                    <?php
-                }
-                // Restore original Post Data
-                wp_reset_postdata();
-                ?>
-            </div>
-        </div>
-    </div>
+	// Loop through rows.
+	while ( have_rows( 'other_sections' ) ) : the_row();
 
-    <div class="bg-white-gradient pb-10 px-5">
-        <?php get_template_part('components/layouts/img-info-block-col'); ?>
-    </div>
+		if ( 0 === $counter % 2 ) {
+			$bg = 'bg-blue-gradient';
+		} else {
+			$bg = 'bg-white-gradient';
+		}
 
-    <div class="bg-blue-gradient">
-        <div class="xl:w-8/12 max-w-screen-2xl mx-auto xl:p-5">
-            <div class="py-10 px-5">
-                <?php get_template_part('components/layouts/faq'); ?>
-            </div>
-        </div>
-    </div>
+		echo "<div class='$bg'>";
+
+		ob_start();  // Start Output Buffering at beginning of loop. This is used to ensure background colors display properly.
+
+		switch ( get_row_layout() ) {
+			case 'text_block':
+				get_template_part( 'components/blocks/text' );
+				break;
+
+			case 'image_banner_text':
+				get_template_part( 'components/blocks/image-text' );
+				break;
+
+			case 'information_section_block':
+				get_template_part( 'components/blocks/information-section' );
+				break;
+
+			case 'event_type_block':
+				get_template_part( 'components/blocks/event-type' );
+				break;
+
+			case 'short_faq_block':
+				get_template_part( 'components/blocks/faq-short' );
+				break;
+
+			case 'instagram_block':
+				get_template_part( 'components/blocks/instagram' );
+				break;
+
+			default:
+				error_log( "Unhandled content block: " . get_row_layout() );
+				break;
+		}
+
+		$output = ob_get_contents();  // Save content of the current loop iteration to $output variable
+		ob_end_clean();  // Dump the content like a bad habit because output has already been saved to variable.
+
+		// Check if the output saved to variable is longer than 1 character (whitespace might return, so 0 ensures nothing does)
+		if ( strlen( $output ) > 1 ) {
+			// Increment counter only if $output had content
+			$counter ++;
+		}
+
+		echo $output;  // Show output saved into variable.
+		echo "</div>";
+
+		// End loop.
+	endwhile;
+endif;
+?>
 
 
 <?php get_footer();
