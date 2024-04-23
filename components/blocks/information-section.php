382c2184d47578117db18933f07bb6843a5d3924
Myzwer <josh@onefortyfivedesign.com>
2024-04-23 15:22:13 -0400

    :bug: Changed button class names a while ago, this never got updated. Fixed that. Buttons now look like they are supposed to.

--- components/blocks/information-section.php
+++ components/blocks/information-section.php
@@ -19,40 +19,40 @@
 <div class="xl:w-8/12 max-w-screen-2xl mx-auto p-5 xl:p-5">
     <div class="grid grid-cols-12 gap-4 md:gap-4">
         <div class="col-span-12 pt-10 text-center mx-auto">
-            <img class="rounded-xl shadow-xl" src="<?php the_sub_field("header_image"); ?>">
+            <img class="rounded-xl shadow-xl" src="<?php the_sub_field( "header_image" ); ?>">
         </div>
 
         <div class="col-span-12 py-5 prose max-w-none">
-            <?php the_sub_field("header_content"); ?>
+			<?php the_sub_field( "header_content" ); ?>
         </div>
     </div>
 
     <div class="grid grid-cols-12 gap-4 md:gap-10">
 
-        <?php
-        if (have_rows('information_section')):
-            while (have_rows('information_section')) : the_row(); ?>
+		<?php
+		if ( have_rows( 'information_section' ) ):
+			while ( have_rows( 'information_section' ) ) : the_row(); ?>
 
                 <div class="col-span-12 md:col-span-6 py-5">
-                    <h2 class="capitalize font-bold text-xl pb-5"><?php the_sub_field('header_title'); ?></h2>
+                    <h2 class="capitalize font-bold text-xl pb-5"><?php the_sub_field( 'header_title' ); ?></h2>
                     <hr class="border-t border-2 border-black">
-                    <p class="pt-5"><?php the_sub_field('content'); ?></p>
+                    <p class="pt-5"><?php the_sub_field( 'content' ); ?></p>
 
-                    <?php if (have_rows('primary_cta')): ?>
-                        <?php while (have_rows('primary_cta')): the_row(); ?>
-                            <a href="<?php the_sub_field("button_link"); ?>">
-                                <button class="ghost mt-3 capitalize">
-                                    <?php the_sub_field("button_text"); ?>
+					<?php if ( have_rows( 'primary_cta' ) ): ?>
+						<?php while ( have_rows( 'primary_cta' ) ): the_row(); ?>
+                            <a href="<?php the_sub_field( "button_link" ); ?>">
+                                <button class="ghost-black mt-3 capitalize">
+									<?php the_sub_field( "button_text" ); ?>
                                 </button>
                             </a>
 
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                    
+						<?php endwhile; ?>
+					<?php endif; ?>
+
                 </div>
 
-            <?php
-            endwhile;
-        endif; ?>
+			<?php
+			endwhile;
+		endif; ?>
     </div>
 </div>
