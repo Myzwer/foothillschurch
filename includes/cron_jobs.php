482ab7ac9be6fd48080d7281ab64aa7bae20fad2
Myzwer <josh@onefortyfivedesign.com>
2024-06-12 15:02:59 -0400

    :scroll: Updated the documentation for the cron jobs.

--- includes/cron_jobs.php
+++ includes/cron_jobs.php
@@ -1,5 +1,13 @@
 <?php
+/**
+ * Hook into the 'wp' action and schedule our event.
+ */
 add_action( 'wp', 'setup_daily_event_draft' );
+
+/**
+ * Schedule the 'unpublish_past_events_daily' event if it isn't already scheduled.
+ * This function will run whenever WordPress loads (on every page load).
+ */
 function setup_daily_event_draft() {
 	if ( ! wp_next_scheduled( 'unpublish_past_events_daily' ) ) {
 		// Setting the time to 1am. If it's past 1am, run the event tomorrow.
@@ -8,11 +16,20 @@ function setup_daily_event_draft() {
 	}
 }
 
+/**
+ * Hook into our custom action and execute the unpublish_past_events function.
+ */
 add_action( 'unpublish_past_events_daily', 'unpublish_past_events' );
+
+/**
+ * Unpublish past events that have a 'removal_date' less than or equal to yesterday's date.
+ * This function will be triggered by our 'unpublish_past_events_daily' event.
+ */
 function unpublish_past_events() {
 	// Get yesterday's date
 	$yesterday = date( 'Ymd', strtotime( '-1 day' ) );
 
+	// Set up query arguments to select all 'event' posts that were supposed to be removed as of yesterday
 	$args = array(
 		'post_type'      => 'event',
 		'posts_per_page' => - 1,
@@ -27,9 +44,11 @@ function unpublish_past_events() {
 		'post_status'    => 'publish'
 	);
 
+	// Query for relevant events
 	$past_events = get_posts( $args );
 
 	if ( $past_events ) {
+		// Loop through each event and update the post status to 'draft'
 		foreach ( $past_events as $event ) {
 			$update_event = array(
 				'ID'          => $event->ID,
