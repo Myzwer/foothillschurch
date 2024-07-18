7d4c438b73cb1e2b78a1244f730d0d0b3435728d
Myzwer <josh@onefortyfivedesign.com>
2024-07-18 10:17:50 -0400

    :repeat: Refactored my terser plugin import to not be inside the logic, instead calling it at the top following best practice.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const StylelintPlugin = require("stylelint-webpack-plugin");
 const CssnanoPlugin = require("cssnano-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 // ***** SET ENVIRONMENT *****
 // * This tells webpack to always run in development mode.
@@ -20,131 +21,129 @@ let mode = "development";
 // * This tells webpack that if we switch the node env (For n00bs: we run 'yarn run prod' in terminal), to switch it to
 // * production mode instead, which minifies, mangles, etc our code.
 if (process.env.NODE_ENV === "production") {
-  mode = "production";
+    mode = "production";
 }
 
 // ***** MODULE EXPORTS *****
 module.exports = {
-  mode,
+    mode,
 
-  module: {
-    rules: [
-      // *** CSS ***
-      {
-        test: /\.s?css$/i,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: "css-loader" },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                config: "./webpack/postcss.config.js",
-              },
-            },
-          },
-          { loader: "resolve-url-loader" },
-          { loader: "sass-loader" },
-        ],
-      },
-
-      // *** BABEL ***
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-
-      // *** FONTS ***
-      {
-        test: /\.(ttf|eot|woff|woff2|svg)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[ext]",
-            outputPath: "../fonts/",
-            esModule: false,
-          },
-        },
-      },
-    ],
-  },
-
-  // * Minimization!
-  // * So this is where CSSNano is configured to minimize our CSS.
-  // * It does a few extra goodies like removing comments and stuff
-  // * Currently using default settings. More on that here:
-  // * https://cssnano.co/docs/optimisations
-  // * This is also where we ping TerserPlugin to minify our JS too.
-  optimization: {
-    minimizer: [
-      new CssnanoPlugin({
-        test: /\.s?css$/i,
-        cssnanoOptions: {
-          preset: [
-            "default",
+    module: {
+        rules: [
+            // *** CSS ***
             {
-              discardComments: { removeAll: true },
+                test: /\.s?css$/i,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader},
+                    {loader: "css-loader"},
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                config: "./webpack/postcss.config.js",
+                            },
+                        },
+                    },
+                    {loader: "resolve-url-loader"},
+                    {loader: "sass-loader"},
+                ],
             },
-          ],
-        },
-      }),
-      (compiler) => {
-        const TerserPlugin = require("terser-webpack-plugin");
-        new TerserPlugin({
-          terserOptions: {
-            compress: {},
-          },
-        }).apply(compiler);
-      },
+
+            // *** BABEL ***
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+
+            // *** FONTS ***
+            {
+                test: /\.(ttf|eot|woff|woff2|svg)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "../fonts/",
+                        esModule: false,
+                    },
+                },
+            },
+        ],
+    },
+
+    // * Minimization!
+    // * So this is where CSSNano is configured to minimize our CSS.
+    // * It does a few extra goodies like removing comments and stuff
+    // * Currently using default settings. More on that here:
+    // * https://cssnano.co/docs/optimisations
+    // * This is also where we ping TerserPlugin to minify our JS too.
+    optimization: {
+        minimizer: [
+            new CssnanoPlugin({
+                test: /\.s?css$/i,
+                cssnanoOptions: {
+                    preset: [
+                        "default",
+                        {
+                            discardComments: {removeAll: true},
+                        },
+                    ],
+                },
+            }),
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {},
+                },
+            }),
+        ],
+    },
+
+    // *** INPUT / OUTPUT ***
+    // * This is how to change entry location as well as output location and name
+    // * Relative to this file, tell it where your js is coming from and where its going.
+    entry: ["./assets/src/js/frontend.js"],
+    output: {
+        filename: "frontend.js",
+        path: path.resolve(__dirname, "assets/public/js"),
+    },
+
+    // ***** SET DEVTOOL *****
+    // * What else can you set this to?
+    // * https://webpack.js.org/configuration/devtool/
+    devtool: "source-map",
+
+    // ***** SET PLUGINS *****
+    plugins: [
+        // *** BROWSERSYNC ***
+        new BrowserSyncPlugin({
+            enable: true, // enable or disable browserSync
+            host: "localhost",
+            port: 3000,
+            mode: "proxy", // proxy | server
+            // * YOU NEED TO CHANGE THIS. VVV
+            // * Local is going to output a Site Domain that ends in .local
+            // * That needs to be pasted here to get Browersync to work properly.
+            proxy: "foothillschurchcom.local",
+            // * CHANGE ME ^^^
+            // BrowserSync will automatically watch for changes to any files connected to our entry,
+            // including both JS and Sass files. We can use this property to tell BrowserSync to watch
+            // for other types of files, in this case PHP files, in our project.
+            files: "**/**/**.php",
+            reload: true,
+        }),
+
+        // *** STYLELINT ***
+        new StylelintPlugin(),
+
+        // *** MINI CSS EXTRACT PLUGIN ***
+        // * Lets get that CSS rolling. This plugin lets Webpack actually build a CSS file that's separate.
+        // * If you want that file to be somewhere else, the path below will change that.
+        // * The path is relative to output.path (the folder where frontend.js is).
+        new MiniCssExtractPlugin({
+            filename: "../css/frontend.css",
+        }),
     ],
-  },
-
-  // *** INPUT / OUTPUT ***
-  // * This is how to change entry location as well as output location and name
-  // * Relative to this file, tell it where your js is coming from and where its going.
-  entry: ["./assets/src/js/frontend.js"],
-  output: {
-    filename: "frontend.js",
-    path: path.resolve(__dirname, "assets/public/js"),
-  },
-
-  // ***** SET DEVTOOL *****
-  // * What else can you set this to?
-  // * https://webpack.js.org/configuration/devtool/
-  devtool: "source-map",
-
-  // ***** SET PLUGINS *****
-  plugins: [
-    // *** BROWSERSYNC ***
-    new BrowserSyncPlugin({
-      enable: true, // enable or disable browserSync
-      host: "localhost",
-      port: 3000,
-      mode: "proxy", // proxy | server
-      // * YOU NEED TO CHANGE THIS. VVV
-      // * Local is going to output a Site Domain that ends in .local
-      // * That needs to be pasted here to get Browersync to work properly.
-      proxy: "foothillschurchcom.local",
-      // * CHANGE ME ^^^
-      // BrowserSync will automatically watch for changes to any files connected to our entry,
-      // including both JS and Sass files. We can use this property to tell BrowserSync to watch
-      // for other types of files, in this case PHP files, in our project.
-      files: "**/**/**.php",
-      reload: true,
-    }),
-
-    // *** STYLELINT ***
-    new StylelintPlugin(),
-
-    // *** MINI CSS EXTRACT PLUGIN ***
-    // * Lets get that CSS rolling. This plugin lets Webpack actually build a CSS file that's separate.
-    // * If you want that file to be somewhere else, the path below will change that.
-    // * The path is relative to output.path (the folder where frontend.js is).
-    new MiniCssExtractPlugin({
-      filename: "../css/frontend.css",
-    }),
-  ],
 };
+
