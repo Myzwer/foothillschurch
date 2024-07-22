6607e25c958595dc9762853e2b8ec7dbfba3d77a
Myzwer <josh@onefortyfivedesign.com>
2024-07-22 10:18:18 -0400

    :repeat: Refactor accordion.js for readability and remove deprecated document.ready.
    Simplified document ready function into shorter syntax and adjusted code nesting for improved readability.

--- assets/src/js/accordion.js
+++ assets/src/js/accordion.js
@@ -1,22 +1,19 @@
 import $ from "jquery";
 
 // Accordion
-const action = 'click';
+const action = "click";
 const speed = "500";
 
-$(document).ready(function () {
-    // Question handler
-    $('.tab-title').on(action, function () {
-        // Get next element
-        $(this).next()
-            .slideToggle(speed)
-            .siblings('.tab-content')
-            .slideUp();
-        // Get arrow for active dropdown
-        const arrow = $(this).children('.fa');
-        // Remove the 'rotate' class for all images except the active.
-        $('.fa').not(arrow).removeClass('rotate');
-        // Toggle rotate class
-        arrow.toggleClass('rotate');
-    });
+$(function () {
+  // Question handler
+  $(".tab-title").on(action, function () {
+    // Get next element
+    $(this).next().slideToggle(speed).siblings(".tab-content").slideUp();
+    // Get arrow for active dropdown
+    const arrow = $(this).children(".fa");
+    // Remove the 'rotate' class for all images except the active.
+    $(".fa").not(arrow).removeClass("rotate");
+    // Toggle rotate class
+    arrow.toggleClass("rotate");
+  });
 });
