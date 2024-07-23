8d7fdf756f2b6c9d386434fe70a20210a0775437
Myzwer <josh@onefortyfivedesign.com>
2024-07-23 09:56:47 -0400

    :repeat: Updated body class application in header.php and fixed indentation
    In the header.php file, the way the body class is assigned has been modified. Instead of applying a hard-coded string class, it now uses WordPress's body_class function to assign the class dynamically. Also fixed the indentation to be more clear.

--- header.php
+++ header.php
@@ -26,80 +26,80 @@
 <!-- Declare html + lang that comes from whatever WP Admin is set to. -->
 <html <?php language_attributes(); ?>>
 
-<!--Start Head-->
-<head>
-    <!--Meta Info-->
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <!--Start Head-->
+    <head>
+        <!--Meta Info-->
+        <meta charset="<?php bloginfo('charset'); ?>">
 
-    <!-- Set viewpoort -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Set viewpoort -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Enables FOAF. -->
-    <!-- https://wordpress.stackexchange.com/questions/173117/why-is-there-a-link-tag-with-rel-profile-pointing-to-gmpg-org-->
-    <link rel="profile" href="https://gmpg.org/xfn/11">
+        <!-- Enables FOAF. -->
+        <!-- https://wordpress.stackexchange.com/questions/173117/why-is-there-a-link-tag-with-rel-profile-pointing-to-gmpg-org-->
+        <link rel="profile" href="https://gmpg.org/xfn/11">
 
-    <!-- Font Awesome Loadin-->
-    <!-- It's Josh Forrester's Kit, so if ever he doesn't work for FC, you need to update this to a new kit -->
-    <script src="https://kit.fontawesome.com/062be7d052.js" crossorigin="anonymous"></script>
+        <!-- Font Awesome Loadin-->
+        <!-- It's Josh Forrester's Kit, so if ever he doesn't work for FC, you need to update this to a new kit -->
+        <script src="https://kit.fontawesome.com/062be7d052.js" crossorigin="anonymous"></script>
 
-    <!--Wordpress Required Stuff-->
-    <?php wp_head(); ?>
-</head>
+        <!--Wordpress Required Stuff-->
+        <?php wp_head(); ?>
+    </head>
 
-<!--Open Body, Apply any Tailwind classes that are global here, Leave WP stuff alone. -->
-<body class="leading-normal tracking-normal" <?php body_class(); ?>>
+    <!--Open Body, Apply any Tailwind classes that are global here, Leave WP stuff alone. -->
+    <body <?php body_class("leading-normal tracking-normal"); ?>>
 
-<!--Wordpress Required Stuff-->
-<?php wp_body_open(); ?>
+        <!--Wordpress Required Stuff-->
+        <?php wp_body_open(); ?>
 
-<!-- Start Topbar code -->
-<!-- The config is in functions.php - The links can be edited in Wp Admin -> Appearance -> Menus. -->
-<!-- THIS DOES NOT USE THE NAVWALKER. -->
-<div class="bg-white">
-    <?php
-    wp_nav_menu(array(
-        'theme_location' => 'top-bar',
-        // 'menu_class'     => 'pass-class', no additional classes needed,  but if they were they go here.
-        'items_wrap' => '<ul class = "topbar">%3$s</ul>'
-    ));
-    ?>
-</div>
-<!-- End Topbar -->
-
-<!--Start Main Navbar-->
-<section class="navigation">
-    <div class="nav-container">
-
-        <!-- Link brand image to homepage. Both the link and img are controlled from Options ACF in WP Admin -->
-        <div class="brand">
-            <a href="<?php the_field('homepage', 'options'); ?>">
-                <img src="<?php the_field('circle_outline_logo', 'options'); ?>"
-                     alt="Foothills Church Logo">
-            </a>
-        </div>
-
-        <!-- Start actual Nav elements-->
-        <nav>
-            <!-- For JS to bind to for mobile functionality -->
-            <div class="nav-mobile">
-                <a id="nav-toggle" href="#!">
-                    <span></span>
-                </a>
-            </div>
-
-            <!-- Start Wordpress Generated Links - Can up updated from Wp Admin -> Appearance -> Menus. -->
-            <!-- This DOES use a custom navwalker found at ./nav_walker.php -->
-            <!-- -->
+        <!-- Start Topbar code -->
+        <!-- The config is in functions.php - The links can be edited in Wp Admin -> Appearance -> Menus. -->
+        <!-- THIS DOES NOT USE THE NAVWALKER. -->
+        <div class="bg-white">
             <?php
             wp_nav_menu(array(
-                'theme_location' => 'main-navigation',
-                'menu_class' => 'primary-menu', // pass whatever classes to be added to top level here
-                'walker' => new PreLaunch_Walker(), // Uses a custom navwalker
-                'items_wrap' => '<ul class="nav-list">%3$s</ul>' // '%3$s' is a WP thing, it adds the content.
+                'theme_location' => 'top-bar',
+                // 'menu_class'     => 'pass-class', no additional classes needed,  but if they were they go here.
+                'items_wrap' => '<ul class = "topbar">%3$s</ul>'
             ));
             ?>
-            <!-- End Wordress -->
-        </nav>
-    </div>
-</section>
-<!--Start Body-->
+        </div>
+        <!-- End Topbar -->
+
+        <!--Start Main Navbar-->
+        <section class="navigation">
+            <div class="nav-container">
+
+                <!-- Link brand image to homepage. Both the link and img are controlled from Options ACF in WP Admin -->
+                <div class="brand">
+                    <a href="<?php the_field('homepage', 'options'); ?>">
+                        <img src="<?php the_field('circle_outline_logo', 'options'); ?>"
+                             alt="Foothills Church Logo">
+                    </a>
+                </div>
+
+                <!-- Start actual Nav elements-->
+                <nav>
+                    <!-- For JS to bind to for mobile functionality -->
+                    <div class="nav-mobile">
+                        <a id="nav-toggle" href="#!">
+                            <span></span>
+                        </a>
+                    </div>
+
+                    <!-- Start Wordpress Generated Links - Can up updated from Wp Admin -> Appearance -> Menus. -->
+                    <!-- This DOES use a custom navwalker found at ./nav_walker.php -->
+                    <!-- -->
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'main-navigation',
+                        'menu_class' => 'primary-menu', // pass whatever classes to be added to top level here
+                        'walker' => new PreLaunch_Walker(), // Uses a custom navwalker
+                        'items_wrap' => '<ul class="nav-list">%3$s</ul>' // '%3$s' is a WP thing, it adds the content.
+                    ));
+                    ?>
+                    <!-- End Wordress -->
+                </nav>
+            </div>
+        </section>
+        <!--Start Body-->
