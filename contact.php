cb0ef6b46fdf101841a6ff00d14f71af7018e44b
Myzwer <josh@onefortyfivedesign.com>
2024-08-01 13:53:04 -0400

    :repeat: Refactor contact.php for improved readability
    Reformatted the conditional if statement to cover hte whole section so nothing shows if there's no content.
    Solves issue #37.

--- contact.php
+++ contact.php
@@ -14,42 +14,43 @@
 
 get_header(); ?>
     <!-- Simple Header-->
-<?php get_template_part('components/headers/simple-header'); ?>
+<?php get_template_part( 'components/headers/simple-header' ); ?>
     <!-- End Header -->
 
     <div class="bg-white-gradient">
         <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
             <div class="col-span-12 text-left mt-10 prose">
-                <img class="rounded-xl shadow-xl" src="<?php the_field('banner_image'); ?>" alt="Contact Image Banner">
+                <img class="rounded-xl shadow-xl" src="<?php the_field( 'banner_image' ); ?>"
+                     alt="Contact Image Banner">
             </div>
 
             <div class="col-span-12 text-left my-10 prose">
-                <?php the_field('contact_content'); ?>
+				<?php the_field( 'contact_content' ); ?>
             </div>
         </div>
     </div>
 
+<?php
+if ( have_rows( 'detail_section' ) ):
+	while ( have_rows( 'detail_section' ) ) : the_row();
+		?>
+        <div class="bg-blue-gradient">
+            <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
 
-    <div class="bg-blue-gradient">
-        <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
-
-            <?php
-            if (have_rows('detail_section')):
-                while (have_rows('detail_section')) : the_row();
-                    ?>
-                    <div class="col-span-12 md:col-span-4 text-left mt-10">
-                        <h3 class="text-2xl md:text-3xl mb-3 font-bold text-center"><?php the_sub_field('title'); ?></h3>
-                        <div class="prose">
-                            <?php the_sub_field('content'); ?>
-                        </div>
+                <div class="col-span-12 md:col-span-4 text-left mt-10">
+                    <h3 class="text-2xl md:text-3xl mb-3 font-bold text-center"><?php the_sub_field( 'title' ); ?></h3>
+                    <div class="prose">
+						<?php the_sub_field( 'content' ); ?>
                     </div>
-                <?php
-                endwhile;
-            endif;
-            ?>
+                </div>
 
+            </div>
         </div>
-    </div>
+
+	<?php
+	endwhile;
+endif;
+?>
 
 
 <?php get_footer();
