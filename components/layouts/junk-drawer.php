6441d4454f885677eaa9b9b72b1dc713abd3d246
Myzwer <josh@onefortyfivedesign.com>
2024-08-01 15:57:49 -0400

    :repeat: Refactor junk-drawer layout for readability
    Simplified the PHP loop structure and improved variable handling in the junk-drawer layout file. Reduced code nesting and enhanced readability.

--- components/layouts/junk-drawer.php
+++ components/layouts/junk-drawer.php
@@ -12,27 +12,20 @@
 ?>
 
 <!-- Start an outer grid. This can be nested inside of a different grid, but it'll look jank. -->
-<?php
-if ( have_rows( 'junk_drawer' ) ):
-while ( have_rows( 'junk_drawer' ) ) :
-the_row(); ?>
-<div class="grid grid-cols-12 gap-4 md:gap-10">
-
-    <div class="col-span-12 md:col-span-6 p-5">
-        <h4 class="text-2xl pb-2 font-bold capitalize"><?php the_sub_field( "title" ); ?></h4>
-        <hr class="h-1 rounded-xl bg-black">
-        <div class="py-3 prose"><?php the_sub_field( "paragraph" ); ?></div>
-		<?php
-		// Hide the button if link is not provided.
-		if ( get_sub_field( 'button_link' ) ):
-			?>
-            <a href="<?php the_sub_field( "button_link" ); ?>" class="ghost-black">
-				<?php the_sub_field( "button_text" ); ?>
-            </a>
-		<?php endif; ?>
+<?php if ( have_rows( 'junk_drawer' ) ) : ?>
+    <div class="grid grid-cols-12 gap-4 md:gap-10">
+		<?php while ( have_rows( 'junk_drawer' ) ) : the_row(); ?>
+            <div class="col-span-12 md:col-span-6 p-5">
+                <h4 class="text-2xl pb-2 font-bold capitalize"><?php the_sub_field( 'title' ); ?></h4>
+                <hr class="h-1 rounded-xl bg-black">
+                <div class="py-3 prose"><?php the_sub_field( 'paragraph' ); ?></div>
+				<?php if ( $button_link = get_sub_field( 'button_link' ) ) : ?>
+                    <a href="<?php echo esc_url( $button_link ); ?>" class="ghost-black">
+						<?php the_sub_field( 'button_text' ); ?>
+                    </a>
+				<?php endif; ?>
+            </div>
+		<?php endwhile; ?>
     </div>
+<?php endif; ?>
 
-	<?php endwhile;
-	endif; ?>
-
-</div>
