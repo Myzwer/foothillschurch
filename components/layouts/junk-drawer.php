967ab1721c96e1823cc14f8eaf2cc7c88c865c42
Myzwer <josh@onefortyfivedesign.com>
2024-08-01 15:19:06 -0400

    :repeat: Refactor grid layout for better structure
    Moved the php if statement outside of the grid so nothing renders if there's no content.

--- components/layouts/junk-drawer.php
+++ components/layouts/junk-drawer.php
@@ -12,24 +12,27 @@
 ?>
 
 <!-- Start an outer grid. This can be nested inside of a different grid, but it'll look jank. -->
+<?php
+if ( have_rows( 'junk_drawer' ) ):
+while ( have_rows( 'junk_drawer' ) ) :
+the_row(); ?>
 <div class="grid grid-cols-12 gap-4 md:gap-10">
-	<?php
-	if ( have_rows( 'junk_drawer' ) ):
-		while ( have_rows( 'junk_drawer' ) ) : the_row(); ?>
-            <div class="col-span-12 md:col-span-6 p-5">
-                <h4 class="text-2xl pb-2 font-bold capitalize"><?php the_sub_field( "title" ); ?></h4>
-                <hr class="h-1 rounded-xl bg-black">
-                <div class="py-3 prose"><?php the_sub_field( "paragraph" ); ?></div>
-				<?php
-				// Hide the button if link is not provided.
-				if ( get_sub_field( 'button_link' ) ):
-					?>
-                    <a href="<?php the_sub_field( "button_link" ); ?>" class="ghost-black">
-						<?php the_sub_field( "button_text" ); ?>
-                    </a>
-				<?php endif; ?>
-            </div>
-		<?php endwhile;
+
+    <div class="col-span-12 md:col-span-6 p-5">
+        <h4 class="text-2xl pb-2 font-bold capitalize"><?php the_sub_field( "title" ); ?></h4>
+        <hr class="h-1 rounded-xl bg-black">
+        <div class="py-3 prose"><?php the_sub_field( "paragraph" ); ?></div>
+		<?php
+		// Hide the button if link is not provided.
+		if ( get_sub_field( 'button_link' ) ):
+			?>
+            <a href="<?php the_sub_field( "button_link" ); ?>" class="ghost-black">
+				<?php the_sub_field( "button_text" ); ?>
+            </a>
+		<?php endif; ?>
+    </div>
+
+	<?php endwhile;
 	endif; ?>
 
 </div>
