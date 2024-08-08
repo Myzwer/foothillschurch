363ca27059941268475305d5d7648ded05fd95e6
Myzwer <josh@onefortyfivedesign.com>
2024-08-08 16:52:01 -0400

    :repeat: Refactor CTA button link handling
    Simplified the logic for deriving the link and HTML attributes in the CTA button components. Added esc_url to sanitize URL outputs and cleaned up comments for better readability.

--- components/headers/default/_button.php
+++ components/headers/default/_button.php
@@ -13,7 +13,7 @@
 <div class="bg-salty-gradient">
     <div class="bg-no-repeat bg-scroll bg-cover relative"
          style="background:
-                 url('<?php echo get_template_directory_uri(); ?>/assets/src/img/topography.png') center center;
+                 url('<?php echo esc_url( get_template_directory_uri() . '/assets/src/img/topography.png' ); ?>') center center;
                  height: 20vh;">
         <div class="content-middle text-center">
             <div class="center add-padding">
@@ -24,35 +24,32 @@
 			<?php if ( have_rows( 'primary_cta' ) ): ?>
 				<?php while ( have_rows( 'primary_cta' ) ): the_row(); ?>
 
-
 					<?php
-					// Retrieve the value of the 'button_link' sub-field, or if it's null, retrieve the value of 'button_link_file'.
-					// Check if 'button_link' has a value; if not, it falls back to 'button_link_file'.
-					$link  = get_sub_field( 'button_link' ) ?: get_sub_field( 'button_link_file' );
-					$tab   = get_sub_field( 'new_tab' );
-					$attrs = null;
-
-					// Check the value of the 'new_tab' field to determine the appropriate HTML attributes to add to the link.
-					if ( $tab == "yes" ) {
-						// If 'new_tab' is set to "yes", set the $attrs variable to open the link in a new browser tab.
-						$attrs = 'target="_blank"';
-					} elseif ( $tab == "cc" ) {
-						// If 'new_tab' is set to "cc", set the $attrs variable to open the link in a Church Center modal.
-						$attrs = "data-open-in-church-center-modal='true'";
-					}
+					$link      = get_sub_field( 'button_link' ) ?? get_sub_field( 'button_link_file' );
 
 					// Hide button if link is returning null
-					if ( $link ): ?>
+					if ( $link ):
+						// Get tab status
+						$new_tab = get_sub_field( 'new_tab' );
+						$attrs = '';
+						if ( $new_tab == "yes" ) {
+							$attrs = 'target="_blank"';
+						} elseif ( $new_tab == "cc" ) {
+							$attrs = "data-open-in-church-center-modal='true'";
+						} ?>
+
                         <div class="mt-3">
-                            <a href="<?php echo $link ?>" <?php echo $attrs ?> class="fab-main-white">
+                            <a href="<?php echo esc_url( $link ); ?>" <?php echo $attrs; ?> class="fab-main-white">
                                 <i class="fa-solid fa-circle-arrow-right"></i> <?php the_sub_field( "button_text" ); ?>
                             </a>
                         </div>
-					<?php endif; ?>
 
-				<?php endwhile;
-			endif; ?>
+					<?php endif; // Close the if $link block ?>
+
+				<?php endwhile; // Close the while loop ?>
+			<?php endif; // Close the if have_rows block ?>
 
         </div>
     </div>
 </div>
+
