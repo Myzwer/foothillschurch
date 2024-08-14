ad1572e9ee62385ef99db2dbf9e3c74c0251ba79
Myzwer <josh@onefortyfivedesign.com>
2024-08-14 09:51:05 -0400

    :bug: Fix an issue where a misplaced while statement generates multiple grid rows.
    When I set the section up to be conditional so it doesn't render if there's no content, I moved the while statement too causing it to be multiple grids instead of a single grid.

--- contact.php
+++ contact.php
@@ -30,27 +30,20 @@ get_header(); ?>
         </div>
     </div>
 
-<?php
-if ( have_rows( 'detail_section' ) ):
-	while ( have_rows( 'detail_section' ) ) : the_row();
-		?>
-        <div class="bg-blue-gradient">
-            <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
-
+<?php if ( have_rows( 'detail_section' ) ): ?>
+    <div class="bg-blue-gradient">
+        <div class="md:w-8/12 mx-auto grid grid-cols-12 p-5 gap-4">
+			<?php while ( have_rows( 'detail_section' ) ) : the_row(); ?>
                 <div class="col-span-12 md:col-span-4 text-left mt-10">
                     <h3 class="text-2xl md:text-3xl mb-3 font-bold text-center"><?php the_sub_field( 'title' ); ?></h3>
                     <div class="prose">
 						<?php the_sub_field( 'content' ); ?>
                     </div>
                 </div>
-
-            </div>
+			<?php endwhile; ?>
         </div>
-
-	<?php
-	endwhile;
-endif;
-?>
+    </div>
+<?php endif; ?>
 
 
 <?php get_footer();
