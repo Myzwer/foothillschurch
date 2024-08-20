492856fdd23dc13f2bf98ee1c7fd5f2aab519a2a
Myzwer <josh@onefortyfivedesign.com>
2024-08-20 15:14:54 -0400

    :bug: Add conditional check for header image in info section
    Wrapped header image in a conditional check to ensure it is only displayed when a header image is provided. This prevents potential display issues when the header image field is left empty.

--- components/blocks/information-section.php
+++ components/blocks/information-section.php
@@ -19,9 +19,11 @@
 
 <div class="xl:w-8/12 max-w-screen-2xl mx-auto p-5 xl:p-5">
     <div class="grid grid-cols-12 gap-4 md:gap-4">
-        <div class="col-span-12 pt-10 text-center mx-auto">
-            <img class="rounded-xl shadow-xl" src="<?php the_sub_field( "header_image" ); ?>" alt="Banner Image">
-        </div>
+		<?php if ( get_sub_field( "header_image" ) ) : ?>
+            <div class="col-span-12 pt-10 text-center mx-auto">
+                <img class="rounded-xl shadow-xl" src="<?php the_sub_field( "header_image" ); ?>" alt="Banner Image">
+            </div>
+		<?php endif; ?>
 
         <div class="col-span-12 py-5 prose max-w-none">
 			<?php the_sub_field( "header_content" ); ?>
