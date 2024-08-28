02bfb4ad713935ed6f71aca3fe52e3cf831139f0
Myzwer <josh@onefortyfivedesign.com>
2024-08-28 09:24:48 -0400

    :zap: Simplify privacy policy template
    Removed redundant conditional check for posts and formatted code for better readability. The simplified template directly outputs the content.

--- privacy-policy.php
+++ privacy-policy.php
@@ -13,28 +13,19 @@
 
 get_header(); ?>
 
-    <!-- Simple Header-->
-<?php get_template_part('components/headers/simple-header'); ?>
-    <!-- End Header -->
-
+<?php get_template_part( 'components/headers/simple-header' ); ?>
 
     <div class="m-4 md:m-10 lg:max-w-6xl lg:mx-auto privacy-policy">
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 p-5 prose">
-                <img class="rounded-xl shadow-xl" src="<?php the_field('banner_image'); ?>"
+                <img class="rounded-xl shadow-xl" src="<?php the_field( 'banner_image' ); ?>"
                      alt="Privacy Policy Banner Image">
             </div>
 
             <div class="col-span-12 p-5 prose">
-                <?php if (have_posts()) : while (have_posts()) : the_post();
-                    the_content();
-                endwhile;
-                else: ?>
-                    <p>Sorry, no posts matched your criteria.</p>
-                <?php endif; ?>
+				<?php the_content(); ?>
             </div>
         </div>
     </div>
-
 
 <?php get_footer();
